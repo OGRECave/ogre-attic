9fa32d7c38764fdc2ef987e166c57ad424495f3b
sinbad <sinbad>
2002-07-13 15:25:59 +0000

    POS_INFINITY and NEG_INFINITY now use numeric_limits<>::infinity()

--- OgreMain/src/OgreMath.cpp
+++ OgreMain/src/OgreMath.cpp
@@ -29,8 +29,8 @@ namespace Ogre
 {
     template<> Math* Singleton<Math>::ms_Singleton = 0;
 
-    const Real Math::POS_INFINITY = std::numeric_limits<Real>::max();
-    const Real Math::NEG_INFINITY = std::numeric_limits<Real>::min();
+    const Real Math::POS_INFINITY = std::numeric_limits<Real>::infinity();
+    const Real Math::NEG_INFINITY = -std::numeric_limits<Real>::infinity();
     const Real Math::PI = Real( 4.0 * atan( 1.0 ) );
     const Real Math::TWO_PI = Real( 2.0 * PI );
     const Real Math::HALF_PI = Real( 0.5 * PI );
