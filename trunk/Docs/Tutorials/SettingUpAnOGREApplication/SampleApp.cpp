20b6606d1c9c6564150a685c76b3bd667221a620
sinbad <sinbad@0896a727-e8d4-4d2b-8422-44d28760243c>
2002-08-01 16:21:18 +0000

    WIP tute on setting up projects for all compilers

---
+++ trunk/Docs/Tutorials/SettingUpAnOGREApplication/SampleApp.cpp
@@ -0,0 +1,104 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://ogre.sourceforge.net
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+http://www.gnu.org/copyleft/gpl.html.
+-----------------------------------------------------------------------------
+*/
+
+/**
+    \file 
+        SampleApp.cpp
+    \brief
+        This file can be used as a starting point for building an OGRE application.
+        I have deliberately compressed both declarations and definitions into 1 file
+        to avoid name dependencies here (so you can easily rename this file), if you
+        get serious you will definitely want to split all this stuff up into separate
+        .h and .cpp files for tidiness, but this is just a jump-start.
+*/
+
+// ----------------------------------------------------------------------------
+// Include the main OGRE header files
+// Ogre.h just expands to including lots of individual OGRE header files
+// ----------------------------------------------------------------------------
+#include "Ogre.h"
+// ----------------------------------------------------------------------------
+// Include the OGRE example framework
+// This includes the classes defined to make getting an OGRE application running
+// a lot easier. It automatically sets up all the main objects and allows you to
+// just override the bits you want to instead of writing it all from scratch.
+// ----------------------------------------------------------------------------
+#include "ExampleApplication.h"
+
+
+// ----------------------------------------------------------------------------
+// Define the application object
+// This is derived from ExampleApplication which is the class OGRE provides to 
+// make it easier to set up OGRE without rewriting the same code all the time.
+// You can override extra methods of ExampleApplication if you want to further
+// specialise the setup routine, otherwise the only mandatory override is the
+// 'createScene' method which is where you set up your own personal scene.
+// ----------------------------------------------------------------------------
+class SampleApp : public ExampleApplication
+{
+public:
+    // Basic constructor
+    SampleApp() {}
+
+protected:
+
+    // Just override the mandatory create scene method
+    void createScene(void)
+    {
+        // FOR YOU TO COMPLETE
+        // Take a look at the demos that come with OGRE in the Samples folder
+        // for an idea of the kind of stuff you can do here
+    }
+
+};
+
+
+// ----------------------------------------------------------------------------
+// Main function, just boots the application object
+// ----------------------------------------------------------------------------
+#if OGRE_PLATFORM == PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+
+
+INT WINAPI WinMain( HINSTANCE hInst, HINSTANCE, LPSTR strCmdLine, INT )
+#else
+int main(int argc, char **argv)
+#endif
+{
+    // Create application object
+    SampleApp app;
+
+    try {
+        app.go();
+    } catch( Exception& e ) {
+#if OGRE_PLATFORM == PLATFORM_WIN32
+        MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
+        fprintf(stderr, "An exception has occured: %s\n",
+                e.getFullDescription().c_str());
+#endif
+    }
+
+
+    return 0;
+}
