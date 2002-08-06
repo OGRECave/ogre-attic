72a9d3badfb7ce36742ecd108060a8895112e0ef
cearny <cearny@0896a727-e8d4-4d2b-8422-44d28760243c>
2002-08-06 05:05:13 +0000

    Added new factory model.

--- trunk/PlugIns/FileSystem/src/FileSystemFactory.cpp
+++ trunk/PlugIns/FileSystem/src/FileSystemFactory.cpp
@@ -1,19 +1,38 @@
 #include "FileSystemFactory.h"
 #include "FileSystem.h"
 
-namespace Ogre {
+BEGIN_OGRE_NAMESPACE
 
-    //-----------------------------------------------------------------------
-    String FileSystemFactory::getArchiveType(void)
-    {
-        return "FileSystem";
-    }
-
-    //-----------------------------------------------------------------------
-    ArchiveEx* FileSystemFactory::createArchive(const String& name)
-    {
-        return new FileSystem(name);
-    }
-
-
+FileSystemFactory::~FileSystemFactory()
+{
 }
+
+//-----------------------------------------------------------------------
+String FileSystemFactory::getArchiveType(void)
+{
+    return "FileSystem";
+}
+
+//-----------------------------------------------------------------------
+ArchiveEx* FileSystemFactory::createArchive(const String& name)
+{
+    return new FileSystem(name);
+}
+
+String FileSystemFactory::getType()
+{
+    return "FileSystem";
+}
+
+ArchiveEx *FileSystemFactory::createObj( int nA, ... )
+{
+    va_list lst;
+    va_start( lst, nA );
+
+    String &name = va_arg( lst, String );
+    return new FileSystem( name );
+
+    va_end( lst );
+}
+
+END_OGRE_NAMESPACE
