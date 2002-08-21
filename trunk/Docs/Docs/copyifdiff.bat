b39e10c2d78409a51e453f46f2a23a53d9a18d95
cearny <cearny@0896a727-e8d4-4d2b-8422-44d28760243c>
2002-08-21 13:31:47 +0000

    Not needed anymore

--- trunk/Docs/Docs/copyifdiff.bat
+++
@@ -1,2 +0,0 @@
-diff %1 ..\..\html\%1
-if errorlevel 1 copy %1 ..\..\html\%1
--- trunk/Docs/Docs/updatedocs.bat
+++
@@ -1,9 +0,0 @@
-cd Config
-doxygen html_update.cfg
-cd ..
-cd temp\html
-copy ..\..\copyifdiff.bat
-for %%d in (*.*) do copyifdiff %%d
-del *.* /Q
-
-
