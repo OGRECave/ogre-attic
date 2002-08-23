a4a44810a54d21578a95400bd96bf1cd7409ae90
sinbad <sinbad>
2002-08-23 20:52:25 +0000

    Turned off debug memory manager for now as it breaks particle systems

--- OgreMain/include/OgreConfig.h
+++ OgreMain/include/OgreConfig.h
@@ -77,6 +77,6 @@ http://www.gnu.org/copyleft/gpl.html.
 /** Set this to 0 if you want to use the standard memory manager in Debug builds
     Release builds always use the standard memory manager
 */
-#define OGRE_DEBUG_MEMORY_MANAGER 1
+#define OGRE_DEBUG_MEMORY_MANAGER 0
 
 #endif
