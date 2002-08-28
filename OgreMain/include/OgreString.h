bb61fb6ef87fb25a82724470a0198608ea4ebd4d
cearny <cearny>
2002-08-28 08:00:37 +0000

    - Removed internationalization stuff since it gave lots of strange errors and is not yet needed. - Added a typedef in class String, StrStreamType which resolves to std::stringstream. Use it!

--- OgreMain/include/OgreString.h
+++ OgreMain/include/OgreString.h
@@ -28,54 +28,71 @@ http://www.gnu.org/copyleft/gpl.html.
 #include "OgrePrerequisites.h"
 
 namespace Ogre {
-    typedef std::basic_string< Char > _StringBase;
-       
-};
 
-#ifdef GCC_3_1
+    typedef std::string _StringBase;
+
+}
+
+// If we're using the GCC 3.1 C++ Std lib
+#if defined( GCC_3_1 )
+
 #include <ext/hash_map>
 namespace __gnu_cxx
 {
-  template <> struct hash<Ogre::_StringBase>
-  {
-    size_t operator()(const Ogre::_StringBase _stringBase) const 
-    { return __stl_hash_string(_stringBase.c_str()); }
-  };
-};
+    template <> struct hash< Ogre::_StringBase >
+    {
+        size_t operator()( const Ogre::_StringBase _stringBase ) const 
+        { 
+            register size_t ret = 0;
+            for( Ogre::_StringBase::const_iterator it = _stringBase.begin(); it != _stringBase.end(); ++it )
+                ret = 5 * ret + *it;
+
+            return ret;
+        }
+    };
+}
+
+// If we're using plain vanilla VC7 Std lib
+#elif !defined( _STLP_HASH_FUN_H )
+
+namespace std 
+{
+    template<> size_t hash_compare< Ogre::_StringBase, std::less< Ogre::_StringBase > >::operator ()( const Ogre::_StringBase& _stringBase ) const
+    {
+        register size_t ret = 0;
+        for( Ogre::_StringBase::const_iterator it = _stringBase.begin(); it != _stringBase.end(); ++it )
+            ret = 5 * ret + *it;
+
+        return ret;
+    }
+}
+
 #endif
 
 namespace Ogre {
 
-#ifdef GCC_3_1
-    typedef ::__gnu_cxx::hash<_StringBase> _StringHash;    
-#else
-    typedef std::hash<_StringBase> _StringHash;
-#endif
-
     /** Wrapper object for String to allow DLL export.
         @note
-            <br>Derived from std::basic_string<char>.
+            Derived from std::string.
     */
     class _OgreExport String : public _StringBase
     {
     public:
+        typedef std::stringstream StrStreamType;
+    public:
         /** Default constructor.
         */
-        String() : 
-          _StringBase() {}
+        String() : _StringBase() {}
 
-        /** Normal copy constructor.
-        */
-        String(const String& rhs) :
-            _StringBase( static_cast<std::string>( rhs ) ) {}
+        String(const String& rhs) : _StringBase( static_cast< const _StringBase& >( rhs ) ) {}
 
         /** Copy constructor for std::string's.
         */
-        String(const _StringBase& rhs) : _StringBase( rhs ) {}
+        String( const _StringBase& rhs ) : _StringBase( rhs ) {}
 
         /** Copy-constructor for C-style strings.
         */
-        String(const char* rhs) : _StringBase( rhs ) {}
+        String( const char* rhs ) : _StringBase( static_cast< const _StringBase& >( rhs ) ) {}
 
         /** Used for interaction with functions that require the old C-style
             strings.
@@ -89,15 +106,17 @@ namespace Ogre {
                 beginning or the end of the String ( the default action is
                 to trim both).
         */
-        void trim(bool left = true, bool right = true);
+        void trim( bool left = true, bool right = true );
 
         /** Returns a StringVector that contains all the substrings delimited
             by the characters in the passed <code>delims</code> argument.
-        @param delims A list of delimiter characters to split by
-        @param maxSplits The maximum number of splits to perform (0 for unlimited splits). If this
-            parameters is > 0, the splitting process will stop after this many splits, left to right.
+            @param 
+                delims A list of delimiter characters to split by
+            @param 
+                maxSplits The maximum number of splits to perform (0 for unlimited splits). If this
+                parameters is > 0, the splitting process will stop after this many splits, left to right.
         */
-        std::vector<String> split( const String& delims = "\t\n ", unsigned int maxSplits = 0) const;
+        std::vector< String > split( const String& delims = "\t\n ", unsigned int maxSplits = 0) const;
 
         /** Upper-cases all the characters in the string.
         */
@@ -112,32 +131,43 @@ namespace Ogre {
             Assumes the only contents of the string are a valid parsable Real. Defaults to  a
             value of 0.0 if conversion is not possible.
         */
-        Real toReal(void);
+        Real toReal(void) const;
+
+        operator _StringBase()
+        {
+            return *this;
+        }
 
         /** Template operator for appending another type into the string. 
-        @remarks
-            Because this operator is templated, you can append any value into a string as
-            long as there is an operator<<(std::basic_iostream, type) or similar method defined somewhere.
-            All the primitive types have this already, and many of the Ogre types do too (see Vector3
-            for an example).
+            @remarks
+                Because this operator is templated, you can append any value into a string as
+                long as there is an operator<<(std::basic_iostream, type) or similar method defined somewhere.
+                All the primitive types have this already, and many of the Ogre types do too (see Vector3
+                for an example).
         */
-        template<typename T> String& operator << (T value)
+        template< typename T > String& operator << (T value)
         {
             // Create stringstream based on *this contents
-            std::stringstream sstr;
+            StrStreamType sstr;
             sstr.str(*this);
             // Seek to end
             sstr.seekp(0, std::ios_base::end);
             // Write
             sstr << value;
             // Assign back
-            *this = sstr.str();
+            *this = _StringBase( sstr.str() );
+
             return *this;
         }
-
-
-
     };
+
+#ifdef GCC_3_1
+    typedef ::__gnu_cxx::hash< _StringBase > _StringHash;    
+#elif !defined( _STLP_HASH_FUN_H )
+    typedef std::hash_compare< _StringBase, std::less< _StringBase > > _StringHash;
+#else
+    typedef std::hash< _StringBase > _StringHash;
+#endif
 
 } // namespace Ogre
 
