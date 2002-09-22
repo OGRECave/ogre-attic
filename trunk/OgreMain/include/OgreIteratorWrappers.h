ab7db54817c748f4a3b8d986c1928efe7d98f09f
sinbad <sinbad@0896a727-e8d4-4d2b-8422-44d28760243c>
2002-09-22 16:34:50 +0000

    Added iterator wrappers for exposing some collections

---
+++ trunk/OgreMain/include/OgreIteratorWrappers.h
@@ -0,0 +1,121 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.stevestreeting.com/ogre/
+
+Copyright © 2000-2001 Steven J. Streeting
+Also see acknowledgements in Readme.html
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+http://www.gnu.org/copyleft/gpl.html.
+-----------------------------------------------------------------------------
+*/
+#ifndef __IteratorWrappers_H__
+#define __IteratorWrappers_H__
+
+#include "OgrePrerequisites.h"
+
+namespace Ogre {
+
+    /** Wraps iteration over a vector.
+    @remarks
+        This class is here just to allow clients to iterate over an internal
+        vector of a class without having to have access to the vector itself
+        (typically to iterate you need both the iterator and the end() iterator
+        to test for the end condition, which is messy).
+        No updates are allowed through this interface, it is purely for 
+        iterating and reading.
+    @par
+        Note that like STL iterators, these iterators are only valid whilst no 
+        updates are made to the underlying collection. You should not attempt to
+        use this iterator if a change is made to the collection. In fact, treat this
+        iterator as a transient object, do NOT store it and try to use it repeatedly.
+    */
+    template <class T>
+    class VectorIterator
+    {
+    private:
+        T::iterator mCurrent;
+        T::iterator mEnd;
+    public:
+        /** Constructor.
+        @remarks
+            Provide a start and end iterator to initialise.
+        */
+        VectorIterator(T::iterator start, T::iterator end)
+            : mCurrent(start), mEnd(end)
+        {
+        }
+
+        /** Returns true if there are more items in the collection. */
+        bool hasMoreElements(void)
+        {
+            return mCurrent != mEnd;
+        }
+
+        /** Returns the next element in the collection, and advances to the next. */
+        T::value_type getNext(void)
+        {
+            return *mCurrent++;
+        }
+
+    };
+
+    /** Wraps iteration over a map.
+    @remarks
+        This class is here just to allow clients to iterate over an internal
+        map of a class without having to have access to the map itself
+        (typically to iterate you need both the iterator and the end() iterator
+        to test for the end condition, which is messy).
+        No updates are allowed through this interface, it is purely for 
+        iterating and reading.
+    @par
+        Note that like STL iterators, these iterators are only valid whilst no 
+        updates are made to the underlying collection. You should not attempt to
+        use this iterator if a change is made to the collection. In fact, treat this
+        iterator as a transient object, do NOT store it and try to use it repeatedly.
+    */
+    template <class T>
+    class MapIterator
+    {
+    private:
+        T::iterator mCurrent;
+        T::iterator mEnd;
+    public:
+        /** Constructor.
+        @remarks
+            Provide a start and end iterator to initialise.
+        */
+        MapIterator(T::iterator start, T::iterator end)
+            : mCurrent(start), mEnd(end)
+        {
+        }
+
+        /** Returns true if there are more items in the collection. */
+        bool hasMoreElements(void)
+        {
+            return mCurrent != mEnd;
+        }
+
+        /** Returns the next value element in the collection, and advances to the next. */
+        T::mapped_type getNext(void)
+        {
+            return (mCurrent++)->second;
+        }
+
+    };
+
+}
+#endif
