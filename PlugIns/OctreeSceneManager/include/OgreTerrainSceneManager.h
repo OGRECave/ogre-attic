d982aaa0d149cfa66c75f76e1e72f6905a5d31c6
sinbad <sinbad>
2002-11-07 23:10:55 +0000

    Added terrain plugin and demo from Jon Anderson

---
+++ PlugIns/OctreeSceneManager/include/OgreTerrainSceneManager.h
@@ -0,0 +1,95 @@
+/***************************************************************************
+                          terrainscenemanager.h  -  description
+                             -------------------
+    begin                : Mon Sep 23 2002
+    copyright            : (C) 2002 by Jon Anderson
+    email                : janders@users.sf.net
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef TERRAINSCENEMANAGER_H
+#define TERRAINSCENEMANAGER_H
+
+#include <OgreOctreeSceneManager.h>
+#include <OgreTerrainRenderable.h>
+
+
+namespace Ogre {
+
+  class Image;
+
+  typedef std::vector< TerrainRenderable *> TerrainRow;
+  typedef std::vector< TerrainRow > Terrain2D;
+
+
+/** This is a basic SceneManager for organizing TerrainRenderables into a total landscape.
+  * It loads a terrain from a .cfg file that specifices what textures/scale/mipmaps/etc to use.
+  *@author Jon Anderson
+  */
+  class TerrainSceneManager : public OctreeSceneManager  {
+  public:
+  TerrainSceneManager( );
+  virtual ~TerrainSceneManager( );
+
+  /** Loads the terrain using parameters int he given config file. */
+  void setWorldGeometry(const String& filename);
+  
+  /** Updates all the TerrainRenderables LOD. */
+  virtual void _updateSceneGraph( Camera * cam );
+
+  /** Aligns TerrainRenderable neighbors, and renders them. */
+  virtual void _renderVisibleObjects( void );
+
+  /** Just the default. */
+  virtual void _findVisibleObjects ( Camera * cam );
+
+  /** Returns the height at the given terrain coordinates. */
+  float getHeightAt( float x, float y );
+
+  /** Returns the normal at the given terrain coordinates. */
+  Vector3 & getNormalAt( float x, float y ){ return mTempNormal;};
+
+
+  protected:
+
+  bool _checkSize(int s )
+    {
+      for(int i=0; i<16; i++ )
+	{
+	  printf( "Checking...%d\n", (1<<i)+1 );
+	  if( s == (1 << i) + 1 )
+	    return true;
+	}
+      return false;
+
+    }
+
+
+  int mNumTiles;
+
+  int mTileSize;
+
+  Vector3 mTempNormal;
+
+  Vector3 mScale;
+
+  Material *mTerrainMaterial;
+
+  SceneNode * mTerrainRoot;
+
+  Terrain2D mTiles;
+
+  Image * mImage;
+
+  };
+
+}
+#endif
--- PlugIns/OctreeSceneManager/src/OgreOctreeSceneManagerDll.cpp
+++ PlugIns/OctreeSceneManager/src/OgreOctreeSceneManagerDll.cpp
@@ -1,22 +1,28 @@
 
 #include <OgreOctreeSceneManager.h>
+#include <OgreTerrainSceneManager.h>
 #include <OgreRoot.h>
 
 namespace Ogre
 {
     OctreeSceneManager* octreePlugin;
+    TerrainSceneManager* terrainPlugin;
 
     extern "C" void dllStartPlugin( void )
     {
         // Create new scene manager
         octreePlugin = new OctreeSceneManager();
+	terrainPlugin = new TerrainSceneManager();
 
         // Register
         Root::getSingleton().setSceneManager( ST_GENERIC, octreePlugin );
+	Root::getSingleton().setSceneManager( ST_EXTERIOR_CLOSE, terrainPlugin );
+	//Root::getSingleton().setSceneManager( ST_EXTERIOR_FAR, terrainPlugin );
     }
 
     extern "C" void dllStopPlugin( void )
     {
         delete octreePlugin;
+	delete terrainPlugin;
     }
 }
