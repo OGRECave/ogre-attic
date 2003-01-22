896f87ad6a93d1ac92a82499ad271487c389253d
cearny <cearny>
2003-01-22 08:04:09 +0000

    First try at render-to-textures.

---
+++ OgreMain/include/OgreRenderTexture.h
@@ -0,0 +1,52 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+(Object-oriented Graphics Rendering Engine)
+For the latest info, see http://ogre.sourceforge.net/
+
+Copyright © 2000-2002 The OGRE Team
+Also see acknowledgements in Readme.html
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+http://www.gnu.org/copyleft/lesser.txt.
+-----------------------------------------------------------------------------
+*/
+#ifndef __RenderTexture_H__
+#define __RenderTexture_H__
+
+#include "OgrePrerequisites.h"
+
+#include "OgreRenderTarget.h"
+
+namespace Ogre
+{
+    class _OgreExport RenderTexture : public RenderTarget
+    {
+    public:
+        RenderTexture( const String & name, uint width, uint height );
+
+    protected:
+		RenderTexture() {};
+		virtual void firePostUpdate();
+
+	protected:
+		/// The texture that gets accesses by the rest of the API.
+        Texture * mTexture;
+
+		/// The texture to which rendering takes place.
+		Texture * mPrivateTex;
+    };
+}
+
+#endif
