9342e2944b740967d0bdb87b7593e2c6ef58a918
cearny <cearny@0896a727-e8d4-4d2b-8422-44d28760243c>
2003-01-22 08:04:45 +0000

    First try at render-to-textures.

---
+++ trunk/OgreMain/src/OgreRenderTexture.cpp
@@ -0,0 +1,48 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+(Object-oriented Graphics Rendering Engine)
+For the latest info, see http://ogre.sourceforge.net/
+
+Copyright © 2000-2002 The OGRE Team
+Also see acknowledgements in Readme.html
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+http://www.gnu.org/copyleft/lesser.txt.
+-----------------------------------------------------------------------------
+*/
+
+#include "OgreRenderTexture.h"
+
+namespace Ogre
+{
+	RenderTexture::RenderTexture( const String & name, uint width, uint height )
+	{
+        mName = name;
+        mWidth = width;
+        mHeight = height;
+		mPriority = 2;
+        mTexture = static_cast< Texture * >( TextureManager::getSingleton().create( mName ) );
+		mPrivateTex = TextureManager::getSingleton().createAsRenderTarget( mName + "_PRIVATE##" );
+        TextureManager::getSingleton().load( static_cast< Resource * >( mTexture ) );
+    }
+
+	void RenderTexture::firePostUpdate()
+	{
+		RenderTarget::firePostUpdate();
+
+		// Copy the newly-rendered data to the public texture surface.
+		mPrivateTex->copyToTexture( mTexture );
+	}
+}
