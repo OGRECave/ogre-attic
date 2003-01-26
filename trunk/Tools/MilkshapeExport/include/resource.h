1b24667483c88ffe542ea8513fa6c48a2161e00b
sinbad <sinbad@0896a727-e8d4-4d2b-8422-44d28760243c>
2003-01-26 10:05:51 +0000

    Resource header update for new options on Milkshape exporter

--- trunk/Tools/MilkshapeExport/include/resource.h
+++ trunk/Tools/MilkshapeExport/include/resource.h
@@ -1,5 +1,5 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
+// Microsoft Developer Studio generated include file.
 // Used by MilkshapeExport.rc
 //
 #define IDD_OPTIONS                     101
@@ -11,14 +11,21 @@
 #define IDC_SPLIT_ANIMATION             1005
 #define IDC_EDIT1                       1006
 #define IDC_FPS                         1006
+#define IDC_GRP_MESH                    1007
+#define IDC_GENERATE_LOD                1008
+#define IDC_LOD_DEPTH                   1009
+#define IDC_LOD_VRQ                     1010
+#define IDC_CBO_LOD_STYLE               1011
+#define IDC_GRP_SKELETON                1012
+#define IDC_NUM_LODS                    1014
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        102
+#define _APS_NEXT_RESOURCE_VALUE        103
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_CONTROL_VALUE         1014
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
