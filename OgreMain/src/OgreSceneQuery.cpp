269311eb325684c6fb4c788efd507e14a4d43022
sinbad <sinbad>
2003-03-26 21:58:08 +0000

    WIP

---
+++ OgreMain/src/OgreSceneQuery.cpp
@@ -0,0 +1,59 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://ogre.sourceforge.net/
+
+Copyright © 2000-2003 The OGRE Team
+Also see acknowledgements in Readme.html
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+http://www.gnu.org/copyleft/lesser.txt.
+-----------------------------------------------------------------------------
+*/
+#include "OgreSceneQuery.h"
+
+namespace Ogre {
+
+    //-----------------------------------------------------------------------
+    SceneQuery::SceneQuery(SceneManager* mgr)
+    : mLastResult(NULL), mParentSceneMgr(mgr)
+    {
+    }
+    //-----------------------------------------------------------------------
+    SceneQuery::~SceneQuery()
+    {
+        clearResults();
+    }
+    //-----------------------------------------------------------------------
+    SceneQueryResult& SceneQuery::getLastResults(void)
+    {
+        assert(mLastResult);
+        return mLastResult;
+    }
+    //-----------------------------------------------------------------------
+    void SceneQuery::clearResults(void)
+    {
+        if (mLastResult)
+        {
+            delete mLastResult;
+        }
+        mLastResult = NULL;
+    }
+
+}
+    
+
+
+
