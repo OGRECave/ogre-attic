08d05aebf268ef283cf40ff275ca1d36d4926eb2
temas <temas@0896a727-e8d4-4d2b-8422-44d28760243c>
2003-04-03 04:18:44 +0000

    Compiles clean again, many updates

---
+++ trunk/Tools/PythonInterface/src/Node.cpp
@@ -0,0 +1,94 @@
+#ifdef HAVE_CONFIG_H
+#   include "config.h"
+#endif
+
+#include "OgrePythonLink.h"
+
+using namespace boost::python;
+using namespace Ogre;
+
+struct NodeWrap : Node
+{
+    NodeWrap(PyObject* self) : _self(self)
+    { }
+    NodeWrap(PyObject* self, const String& name) : _self(self)
+    { }
+    Node* createChildImpl(void)
+    { return call_method<Node*>(_self, "createChildImpl"); }
+    Node* createChildImpl(const String& name)
+    { return call_method<Node*>(_self, "createChildImpl", name); }
+    PyObject* _self;
+};
+
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(Node_createChild2_stubs, createChild,1,2)
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(Node_createChild3_stubs, createChild,1,3)
+
+void exportNode()
+{
+    class_<Node, NodeWrap, boost::noncopyable>("Node",  init<>())
+        .def(init<const String&>())
+        .def("getName", &Node::getName, ccr())
+        .def("getParent", &Node::getParent, rir())
+        .def("getOrientation", &Node::getOrientation, ccr())
+        .def("setOrientation", (void (Node::*) (const Quaternion&)) 
+            &Node::setOrientation)
+        .def("setOrientation", (void (Node::*) (Real, Real, Real, Real)) 
+            &Node::setOrientation)
+        .def("resetOrientation", &Node::resetOrientation)
+        .def("setPosition", (void (Node::*) (const Vector3&)) 
+            &Node::setPosition)
+        .def("setPosition", (void (Node::*) (Real, Real, Real)) 
+            &Node::setPosition)
+        .def("getPosition", &Node::getPosition, ccr())
+        .def("setScale", (void (Node::*) (const Vector3&)) &Node::setScale)
+        .def("setScale", (void (Node::*) (Real, Real, Real)) &Node::setScale)
+        .def("getScale", &Node::getScale, ccr())
+        .def("setInheritScale", &Node::setInheritScale)
+        .def("getInheritScale", &Node::getInheritScale)
+        .def("scale", (void (Node::*) (const Vector3&)) &Node::scale)
+        .def("scale", (void (Node::*) (Real, Real, Real)) &Node::scale)
+        .def("translate", (void (Node::*) (const Vector3&)) &Node::translate)
+        .def("translate", (void (Node::*) (Real, Real, Real)) &Node::translate)
+        .def("translate", (void (Node::*) (const Matrix3&, const Vector3&)) 
+            &Node::translate)
+        .def("translate", (void (Node::*) (const Matrix3&, Real, Real, Real)) 
+            &Node::translate)
+        .def("roll", &Node::roll)
+        .def("pitch", &Node::pitch)
+        .def("yaw", &Node::yaw)
+        .def("rotate", (void (Node::*) (const Vector3&, Real)) &Node::rotate)
+        .def("rotate", (void (Node::*) (const Quaternion&)) &Node::rotate)
+        .def("getLocalAxes", &Node::getLocalAxes)
+        .def("createChild", (Node* (Node::*) (const Vector3&, 
+            const Quaternion&)) &Node::createChild,
+            Node_createChild2_stubs()[rir()])
+        .def("createChild", (Node* (Node::*) (const String&, const Vector3&, 
+            const Quaternion&)) &Node::createChild,
+            Node_createChild3_stubs()[rir()])
+        .def("addChild", &Node::addChild)
+        .def("numChildren", &Node::numChildren)
+        .def("getChild", (Node* (Node::*) (unsigned short) const) 
+                &Node::getChild, rir())
+        .def("getChild", (Node* (Node::*) (const String&) const) 
+                &Node::getChild, rir())
+        // XXX getChildIterator support
+        .def("removeChild", (Node* (Node::*) (unsigned short)) 
+            &Node::removeChild, rir())
+        .def("removeChild", (Node* (Node::*) (const String&)) 
+            &Node::removeChild, rir())
+        .def("removeAllChildren", &Node::removeAllChildren)
+        .def("getMaterial", &Node::getMaterial, rir())
+        .def("getRenderOperation", &Node::getRenderOperation)
+        .def("getWorldTransforms", &Node::getWorldTransforms)
+        .def("setInitialState", &Node::setInitialState)
+        .def("resetToInitialState", &Node::resetToInitialState)
+        .def("getInitialPosition", &Node::getInitialPosition, ccr())
+        .def("getInitialOrientation", &Node::getInitialOrientation, ccr())
+        .def("getInitialScale", &Node::getInitialScale, ccr())
+        .def("getSquaredViewDepth", &Node::getSquaredViewDepth)
+        .def("needUpdate", &Node::needUpdate)
+        .def("requestUpdate", &Node::requestUpdate)
+        .def("cancelUpdate", &Node::cancelUpdate)
+    ;
+}
+
--- trunk/Tools/PythonInterface/src/SceneNode.cpp
+++ trunk/Tools/PythonInterface/src/SceneNode.cpp
@@ -7,43 +7,32 @@
 using namespace boost::python;
 using namespace Ogre;
 
-BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(SceneNode_createChild_stubs, createChild,1,3)
-
 void exportSceneNode()
 {
-    class_<SceneNode>("SceneNode", init<SceneManager*>())
-        .def("createChild", (SceneNode* (SceneNode::*)(const String&, const
-                        Vector3&, const Quaternion&))&SceneNode::createChild,
-                SceneNode_createChild_stubs()[rir()])
-        .def("attachCamera", &SceneNode::attachCamera)
+    class_<SceneNode, bases<Node> >("SceneNode", init<SceneManager*>())
+        .def(init<SceneManager*, const String&>())
         .def("attachObject", &SceneNode::attachObject)
-        .def("attachLight", &SceneNode::attachLight)
-        .def("detachObject", (MovableObject* (SceneNode::*)(const String&))
-                &SceneNode::detachObject, rir())
-        .def("getAttachedObject", (MovableObject* (SceneNode::*)(const String&))
-                &SceneNode::getAttachedObject, rir())
-        .def("getChild", (SceneNode* (SceneNode::*)(const String&) const)
-                &SceneNode::getChild, rir())
-        .def("getLocalAxes", &SceneNode::getLocalAxes)
-        .def("getOrientation", &SceneNode::getOrientation, ccr())
-        .def("getPosition", &SceneNode::getPosition, ccr())
         .def("numAttachedObjects", &SceneNode::numAttachedObjects)
-        .def("numChildren", &SceneNode::numChildren)
-        .def("pitch", &SceneNode::pitch)
-        .def("removeChild",(SceneNode* (SceneNode::*)(const String&))
-                &SceneNode::removeChild, rir())
-        .def("resetOrientation", &SceneNode::resetOrientation)
-        .def("roll", &SceneNode::roll)
-        .def("rotate", (void (SceneNode::*)(const Vector3&,Real))&SceneNode::rotate)
-        .def("rotate", (void (SceneNode::*)(const Quaternion&))&SceneNode::rotate)
-        .def("setOrientation", &SceneNode::setOrientation)
-        .def("setPosition", (void (SceneNode::*)(const Vector3&))&SceneNode::setPosition)
-        .def("setPosition", (void (SceneNode::*)(Real,Real,Real))&SceneNode::setPosition)
-        .def("translate", (void (SceneNode::*)(const Vector3&))&SceneNode::translate)
-        .def("translate", (void (SceneNode::*)(Real,Real,Real))&SceneNode::translate)
-        .def("translate", (void (SceneNode::*)(const Matrix3&, const Vector3&))&SceneNode::translate)
-        .def("translate", (void (SceneNode::*)(const Matrix3&, Real,Real,Real))&SceneNode::translate)
-        .def("yaw", &SceneNode::yaw)
+        .def("getAttachedObject", 
+                (MovableObject* (SceneNode::*) (const String&)) 
+                &SceneNode::getAttachedObject, rir())
+        .def("getAttachedObject", 
+                (MovableObject* (SceneNode::*) (unsigned short)) 
+                &SceneNode::getAttachedObject, rir())
+        .def("detachObject", (MovableObject* (SceneNode::*) (unsigned short)) 
+                    &SceneNode::detachObject, rir())
+        .def("detachObject", (MovableObject* (SceneNode::*) (const String&))
+                    &SceneNode::detachObject, rir())
+        .def("detachAllObjects", &SceneNode::detachAllObjects)
+        // XXX getAttachedObjectIterator
+        .def("getCreator", &SceneNode::getCreator, rir())
+        .def("removeAndDestroyChild", (void (SceneNode::*) (const String&)) 
+                &SceneNode::removeAndDestroyChild)
+        .def("removeAndDestroyChild", (void (SceneNode::*) (unsigned short)) 
+                &SceneNode::removeAndDestroyChild)
+        .def("removeAndDestroyAllChildren", &SceneNode::removeAndDestroyAllChildren)
+        .def("showBoundingBox", &SceneNode::showBoundingBox)
+        .def("getShowBoundingBox", &SceneNode::getShowBoundingBox)
     ;
 }
 
