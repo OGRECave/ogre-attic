62cbd825bca53432ce8729d619f7441843a19d45
sinbad <sinbad@0896a727-e8d4-4d2b-8422-44d28760243c>
2003-04-12 14:28:52 +0000

    Added missing files for SDLExtensionManager (doh)

---
+++ trunk/RenderSystems/SDL/src/OgreSDLExtensionManager.cpp
@@ -0,0 +1,61 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://ogre.sourceforge.net/
+
+Copyright © 2000-2002 The OGRE Team
+Also see acknowledgements in Readme.html
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+http://www.gnu.org/copyleft/lesser.txt.
+-----------------------------------------------------------------------------
+*/
+
+#include "OgreSDLExtensionManager.h"
+
+#include <sstream>
+
+namespace Ogre {
+
+    template<> SDLExtensionManager* Singleton<SDLExtensionManager>::ms_Singleton = 0;
+
+    SDLExtensionManager::SDLExtensionManager()
+    { 
+        std::stringstream ext;
+        String str;
+		
+        ext << glGetString(GL_EXTENSIONS);
+
+        while(ext >> str)
+        {
+            extensionList.insert(str);
+        }
+
+        ext.str("");
+    }
+
+    bool SDLExtensionManager::checkExtension(const String& str)
+    {
+        if(extensionList.find(str) == extensionList.end())
+            return false;
+
+        return true;
+    }
+
+    SDLExtensionManager& SDLExtensionManager::getSingleton(void)
+    {
+        return Singleton<SDLExtensionManager>::getSingleton();
+    }
+};
