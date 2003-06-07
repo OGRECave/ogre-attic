70d76b234b5b12ba481af4a6301aac7420d075b4
sinbad <sinbad>
2003-06-07 23:45:33 +0000

    Increased the default range since the default lights never attenuate so the behaviour between D3D (which uses the range) and GL (which doesn't) changes a bit too close so creates differences.

--- OgreMain/src/OgreLight.cpp
+++ OgreMain/src/OgreLight.cpp
@@ -58,7 +58,7 @@ namespace Ogre {
         mLightType = LT_POINT;
         mDiffuse = ColourValue::White;
         mSpecular = ColourValue::Black;
-        mRange = 1000;
+        mRange = 100000;
         mAttenuationConst = 1.0f;
         mAttenuationLinear = 0.0f;
         mAttenuationQuad = 0.0f;
