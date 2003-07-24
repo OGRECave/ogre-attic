ce87648263ada141cbe7d6de76af2a31e589281c
sinbad <sinbad>
2003-07-24 21:54:16 +0000

    Added emulated hardware vertex / index buffers & manager, to be used by MeshSerializer (exporters) at least since rendersystem will not have been initialised, possibly also for GL when VBO extension is not available and maybe D3D7.

---
+++ OgreMain/src/OgreDefaultHardwareBufferManager.cpp
@@ -0,0 +1,140 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://ogre.sourceforge.net/
+
+Copyright © 2000-2003 The OGRE Team
+Also see acknowledgements in Readme.html
+
+This program is free software you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free Software
+Foundation either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with
+this program if not, write to the Free Software Foundation, Inc., 59 Temple
+Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+http://www.gnu.org/copyleft/lesser.txt.
+-----------------------------------------------------------------------------
+*/
+#include "OgreDefaultHardwareBufferManager.h"
+
+namespace Ogre {
+
+	DefaultHardwareVertexBuffer::DefaultHardwareVertexBuffer(size_t vertexSize, size_t numVertices, 
+		HardwareBuffer::Usage usage) : HardwareVertexBuffer(vertexSize, numVertices, usage)
+	{
+		mpData = new unsigned char[mSizeInBytes];
+	}
+	//-----------------------------------------------------------------------
+    DefaultHardwareVertexBuffer::~DefaultHardwareVertexBuffer()
+	{
+		delete [] mpData;
+	}
+	//-----------------------------------------------------------------------
+    void* DefaultHardwareVertexBuffer::lock(size_t offset, size_t length, LockOptions options)
+	{
+		return mpData + offset;
+		mIsLocked = true;
+	}
+	//-----------------------------------------------------------------------
+	void DefaultHardwareVertexBuffer::unlock(void)
+	{
+		mIsLocked = false;
+	}
+	//-----------------------------------------------------------------------
+    void DefaultHardwareVertexBuffer::readData(size_t offset, size_t length, void* pDest)
+	{
+		assert((offset + length) <= mSizeInBytes);
+		memcpy(pDest, mpData + offset, length);
+	}
+	//-----------------------------------------------------------------------
+    void DefaultHardwareVertexBuffer::writeData(size_t offset, size_t length, const void* pSource,
+			bool discardWholeBuffer)
+	{
+		assert((offset + length) <= mSizeInBytes);
+		// ignore discard, memory is not guaranteed to be zeroised
+		memcpy(mpData + offset, pSource, length);
+
+	}
+	//-----------------------------------------------------------------------
+
+	DefaultHardwareIndexBuffer::DefaultHardwareIndexBuffer(IndexType idxType, 
+		size_t numIndexes, HardwareBuffer::Usage usage) 
+		: HardwareIndexBuffer(idxType, numIndexes, usage)
+	{
+		mpData = new unsigned char[mSizeInBytes];
+	}
+	//-----------------------------------------------------------------------
+    DefaultHardwareIndexBuffer::~DefaultHardwareIndexBuffer()
+	{
+		delete [] mpData;
+	}
+	//-----------------------------------------------------------------------
+    void* DefaultHardwareIndexBuffer::lock(size_t offset, size_t length, LockOptions options)
+	{
+		return mpData + offset;
+		mIsLocked = true;
+	}
+	//-----------------------------------------------------------------------
+	void DefaultHardwareIndexBuffer::unlock(void)
+	{
+		mIsLocked = false;
+	}
+	//-----------------------------------------------------------------------
+    void DefaultHardwareIndexBuffer::readData(size_t offset, size_t length, void* pDest)
+	{
+		assert((offset + length) <= mSizeInBytes);
+		memcpy(pDest, mpData + offset, length);
+	}
+	//-----------------------------------------------------------------------
+    void DefaultHardwareIndexBuffer::writeData(size_t offset, size_t length, const void* pSource,
+			bool discardWholeBuffer)
+	{
+		assert((offset + length) <= mSizeInBytes);
+		// ignore discard, memory is not guaranteed to be zeroised
+		memcpy(mpData + offset, pSource, length);
+
+	}
+	//-----------------------------------------------------------------------
+	
+	
+	//-----------------------------------------------------------------------
+	void DefaultHardwareBufferManager::destroyVertexBuffer(HardwareVertexBuffer* buf)
+	{
+		delete buf;
+	}
+    //-----------------------------------------------------------------------
+	void DefaultHardwareBufferManager::destroyIndexBuffer(HardwareIndexBuffer* buf)
+	{
+		delete buf;
+	}
+    //-----------------------------------------------------------------------
+    DefaultHardwareBufferManager::DefaultHardwareBufferManager()
+	{
+	}
+    //-----------------------------------------------------------------------
+    DefaultHardwareBufferManager::~DefaultHardwareBufferManager()
+	{
+	}
+    //-----------------------------------------------------------------------
+	HardwareVertexBufferSharedPtr 
+        DefaultHardwareBufferManager::createVertexBuffer(size_t vertexSize, size_t numVerts, HardwareBuffer::Usage usage)
+	{
+		return HardwareVertexBufferSharedPtr(
+			new DefaultHardwareVertexBuffer(vertexSize, numVerts, usage));
+	}
+    //-----------------------------------------------------------------------
+	HardwareIndexBufferSharedPtr 
+        DefaultHardwareBufferManager::createIndexBuffer(HardwareIndexBuffer::IndexType itype, 
+		size_t numIndexes, HardwareBuffer::Usage usage)
+	{
+		return HardwareIndexBufferSharedPtr(
+			new DefaultHardwareIndexBuffer(itype, numIndexes, usage) );
+	}
+}
