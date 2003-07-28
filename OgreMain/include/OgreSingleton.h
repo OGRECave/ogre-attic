91652c07d8d612122a4b9e8b2d9ea1b307c233a4
cearny <cearny>
2003-07-28 22:17:06 +0000

    #define'd out the nasty pointer math hack in the Singleton constructor to a fall-back since it was only needed by VC5.

--- OgreMain/include/OgreSingleton.h
+++ OgreMain/include/OgreSingleton.h
@@ -57,8 +57,12 @@ namespace Ogre {
         Singleton( void )
         {
             assert( !ms_Singleton );
+#if defined( _MSC_VER ) && _MSC_VER < 1200	 
             int offset = (int)(T*)1 - (int)(Singleton <T>*)(T*)1;
             ms_Singleton = (T*)((int)this + offset);
+#else
+	    ms_Singleton = static_cast< T* >( this );
+#endif
         }
         ~Singleton( void )
             {  assert( ms_Singleton );  ms_Singleton = 0;  }
