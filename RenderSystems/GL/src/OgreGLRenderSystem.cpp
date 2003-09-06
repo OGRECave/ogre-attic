b99b94cf20d1a9db359e7f13d5637d98783afb01
sinbad <sinbad>
2003-09-06 00:34:47 +0000

    glDrawElements -> glDrawRangeElements

--- RenderSystems/GL/src/OgreGLRenderSystem.cpp
+++ RenderSystems/GL/src/OgreGLRenderSystem.cpp
@@ -1655,9 +1655,9 @@ namespace Ogre {
             }
 
             GLenum indexType = (op.indexData->indexBuffer->getType() == HardwareIndexBuffer::IT_16BIT) ? GL_UNSIGNED_SHORT : GL_UNSIGNED_INT;
-
-            glDrawElements( primType, op.indexData->indexCount, indexType,
-                pBufferData);
+            glDrawRangeElements(primType, op.indexData->indexStart, 
+                op.indexData->indexStart + op.indexData->indexCount - 1,
+                op.indexData->indexCount, indexType, pBufferData);
 
         }
         else
