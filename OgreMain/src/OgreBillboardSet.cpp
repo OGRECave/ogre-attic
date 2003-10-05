d145a6a616ad578d30f21017c6a3b32f565ab515
sinbad <sinbad>
2003-10-05 11:43:44 +0000

    Bugfix for getBillboard and removeBillboard when index is in the latter half of the list, merged from 0.12.0 branch

--- OgreMain/src/OgreBillboardSet.cpp
+++ OgreMain/src/OgreBillboardSet.cpp
@@ -155,6 +155,7 @@ namespace Ogre {
         ActiveBillboardList::const_iterator it;
         if( index >= ( mActiveBillboards.size() >> 1 ) )
         {
+			index = static_cast<unsigned int>(mActiveBillboards.size()) - index;
             for( it = mActiveBillboards.end(); index; --index, --it );
         }
         else
@@ -181,6 +182,7 @@ namespace Ogre {
         ActiveBillboardList::iterator it;
         if( index >= ( mActiveBillboards.size() >> 1 ) )
         {
+			index = static_cast<unsigned int>(mActiveBillboards.size()) - index;
             for( it = mActiveBillboards.end(); index; --index, --it );
         }
         else
