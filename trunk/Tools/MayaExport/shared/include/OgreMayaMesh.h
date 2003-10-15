131dfe967f903270b4df03da950c122a0327d8a4
bytelord <bytelord@0896a727-e8d4-4d2b-8422-44d28760243c>
2003-10-15 16:11:25 +0000

    changed file headers to GPL bugfix: use material prefix in submesh's material name

--- trunk/Tools/MayaExport/shared/include/OgreMayaMesh.h
+++ trunk/Tools/MayaExport/shared/include/OgreMayaMesh.h
@@ -1,23 +1,24 @@
 /*
-===============================================================================
+============================================================================
 This source file is part of the Ogre-Maya Tools.
 Distributed as part of Ogre (Object-oriented Graphics Rendering Engine).
+Copyright (C) 2003 Fifty1 Software Inc., Bytelords
 
-This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU Lesser General Public License as published by the Free 
-Software Foundation; either version 2 of the License, or (at your option) any 
-later version.
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
-This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more 
-details.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-You should have received a copy of the GNU Lesser General Public License along 
-with this program; if not, write to the Free Software Foundation, Inc., 
-59 Temple Place - Suite 330, Boston, MA 02111-1307, USA, or go to
-http://www.gnu.org/copyleft/lesser.txt.
-===============================================================================
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+or go to http://www.gnu.org/licenses/gpl.txt
+============================================================================
 */
 #ifndef _OGREMAYA_MESH_H_
 #define _OGREMAYA_MESH_H_
