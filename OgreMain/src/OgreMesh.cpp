72196d1c1828c9edf53ce84e35d13a055f085a1e
sinbad <sinbad>
2003-10-17 20:24:23 +0000

    Bugfix - normalisation of bone weights was only happening when they already equalled 1 - inverted condition (doh)

--- OgreMain/src/OgreMesh.cpp
+++ OgreMain/src/OgreMesh.cpp
@@ -452,7 +452,7 @@ namespace Ogre {
                 totalWeight += i->second.weight;
             }
             // Now normalise if total weight is outside tolerance
-            if (Math::RealEqual(totalWeight, 1.0f))
+            if (!Math::RealEqual(totalWeight, 1.0f))
             {
                 for (i = normalise_range.first; i != normalise_range.second; ++i)
                 {
