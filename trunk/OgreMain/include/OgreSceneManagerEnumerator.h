1b7f2f3a82be74efe3daea15e7a0fe84c2915129
sinbad <sinbad@0896a727-e8d4-4d2b-8422-44d28760243c>
2003-11-22 02:06:19 +0000

    Added extra scene type required for paging landscape

--- trunk/OgreMain/include/OgreSceneManagerEnumerator.h
+++ trunk/OgreMain/include/OgreSceneManagerEnumerator.h
@@ -39,6 +39,7 @@ namespace Ogre {
         ST_GENERIC,
         ST_EXTERIOR_CLOSE,
         ST_EXTERIOR_FAR,
+        ST_EXTERIOR_REAL_FAR,
         ST_INTERIOR
     };
     /** Enumerates the SceneManager classes available to applications.
