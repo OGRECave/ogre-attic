a495bd30ee869f5f152cbbf115a4f74e2137c712
sinbad <sinbad@0896a727-e8d4-4d2b-8422-44d28760243c>
2004-03-03 12:30:09 +0000

    Hardware skinning support in GL Added hardware skinning support to robot material Extended the skeletal animation demo to report hardware / software skinning and use more robots

--- trunk/Samples/SkeletalAnimation/include/SkeletalAnimation.h
+++ trunk/Samples/SkeletalAnimation/include/SkeletalAnimation.h
@@ -22,22 +22,26 @@ Description: Specialisation of OGRE's framework application to show the
 
 #include "ExampleApplication.h"
 
+#define NUM_ROBOTS 10
+AnimationState* mAnimState[NUM_ROBOTS];
+Real mAnimationSpeed[NUM_ROBOTS];
 
 // Event handler to animate
 class SkeletalAnimationFrameListener : public ExampleFrameListener
 {
 protected:
-    AnimationState* mAnimState;
 public:
-    SkeletalAnimationFrameListener(RenderWindow* win, Camera* cam, AnimationState* anim)
+    SkeletalAnimationFrameListener(RenderWindow* win, Camera* cam)
         : ExampleFrameListener(win, cam)
     {
-        mAnimState = anim;
     }
 
     bool frameStarted(const FrameEvent& evt)
     {
-        mAnimState->addTime(evt.timeSinceLastFrame);
+        for (int i = 0; i < NUM_ROBOTS; ++i)
+        {
+            mAnimState[i]->addTime(evt.timeSinceLastFrame * mAnimationSpeed[i]);
+        }
 
         // Call default
         return ExampleFrameListener::frameStarted(evt);
@@ -53,7 +57,6 @@ public:
     SkeletalApplication() {}
 
 protected:
-    AnimationState* mAnimState;
 
     // Just override the mandatory create scene method
     void createScene(void)
@@ -65,14 +68,20 @@ protected:
         mSceneMgr->setAmbientLight(ColourValue(0.5, 0.5, 0.5));
 
 
-       
-        Entity *ent = mSceneMgr->createEntity("robot", "robot.mesh");
+        
+        Entity *ent;
+        for (int i = 0; i < NUM_ROBOTS; ++i)
+        {
+            ent = mSceneMgr->createEntity("robot" + StringConverter::toString(i), "robot.mesh");
+            // Add entity to the scene node
+            mSceneMgr->getRootSceneNode()->createChildSceneNode(
+                Vector3(0,0,(i*50)-(NUM_ROBOTS*50/2)))->attachObject(ent);
+            mAnimState[i] = ent->getAnimationState("Walk");
+            mAnimState[i]->setEnabled(true);
+            mAnimationSpeed[i] = Math::RangeRandom(0.5, 1.5);
+        }
 
-        // Add entity to the root scene node
-        mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(ent);
 
-        mAnimState = ent->getAnimationState("Walk");
-        mAnimState->setEnabled(true);
 
         // Give it a little ambience with lights
         Light* l;
@@ -88,13 +97,28 @@ protected:
         mCamera->setPosition(100,50,100);
         mCamera->lookAt(-50,50,0);
 
+        // Report whether hardware skinning is enabled or not
+        Technique* t = ent->getSubEntity(0)->getMaterial()->getBestTechnique();
+        Pass* p = t->getPass(0);
+        if (p->hasVertexProgram() && 
+            p->getVertexProgram()->isSkeletalAnimationIncluded())
+        {
+            mWindow->setDebugText("Hardware skinning is enabled");
+        }
+        else
+        {
+            mWindow->setDebugText("Software skinning is enabled");
+        }
+
+
+
 
     }
 
     // Create new frame listener
     void createFrameListener(void)
     {
-        mFrameListener= new SkeletalAnimationFrameListener(mWindow, mCamera, mAnimState);
+        mFrameListener= new SkeletalAnimationFrameListener(mWindow, mCamera);
         mRoot->addFrameListener(mFrameListener);
     }
 
