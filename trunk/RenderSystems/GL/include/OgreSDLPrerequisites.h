343496cef2d223596e84d4371a0b5687b1248170
josullivan <josullivan@0896a727-e8d4-4d2b-8422-44d28760243c>
2004-09-26 16:36:26 +0000

    Included "OgrePlatform.h" so that the platform check wouldn't fail.

--- trunk/RenderSystems/GL/include/OgreSDLPrerequisites.h
+++ trunk/RenderSystems/GL/include/OgreSDLPrerequisites.h
@@ -25,6 +25,8 @@ http://www.gnu.org/copyleft/lesser.txt.
 #ifndef __SDLPrerequisites_H__
 #define __SDLPrerequisites_H__
 
+#include "OgrePlatform.h"
+
 #if OGRE_PLATFORM == PLATFORM_APPLE
 #	include <SDL/sdl.h>
 #else
