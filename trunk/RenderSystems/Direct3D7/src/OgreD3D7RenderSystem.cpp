d10a56ec306b54b5ce951369a72621e68f82aeba
sinbad <sinbad@0896a727-e8d4-4d2b-8422-44d28760243c>
2004-10-05 23:57:30 +0000

    Fixed oblique projection in D3D - I forgot I had to include right-handed coords conversion

--- trunk/RenderSystems/Direct3D7/src/OgreD3D7RenderSystem.cpp
+++ trunk/RenderSystems/Direct3D7/src/OgreD3D7RenderSystem.cpp
@@ -2761,21 +2761,30 @@ namespace Ogre {
         // transform it into camera space by multiplying it
         // by the inverse of the projection matrix
 
+        /* generalised version
+        Vector4 q = matrix.inverse() * 
+        Vector4(Math::Sign(plane.normal.x), Math::Sign(plane.normal.y), 1.0f, 1.0f);
+        */
         Vector4 q;
         q.x = Math::Sign(plane.normal.x) / matrix[0][0];
         q.y = Math::Sign(plane.normal.y) / matrix[1][1];
-        q.z = 1.0F;
-        q.w = (1.0F - matrix[2][2]) / matrix[3][2];
+        q.z = 1.0F; 
+        // flip the next bit from Lengyel since we're right-handed
+        //q.w = (1.0F - matrix[2][2]) / matrix[2][3];
+        q.w = (1.0F + matrix[2][2]) / matrix[2][3];
 
         // Calculate the scaled plane vector
         Vector4 clipPlane4d(plane.normal.x, plane.normal.y, plane.normal.z, plane.d);
         Vector4 c = clipPlane4d * (1.0F / (clipPlane4d.dotProduct(q)));
 
-        // Replace the third column of the projection matrix
-        matrix[0][2] = c.x;
-        matrix[1][2] = c.y;
-        matrix[2][2] = c.z;
-        matrix[3][2] = c.w; 
+        // Replace the third row of the projection matrix
+        matrix[2][0] = c.x;
+        matrix[2][1] = c.y;
+        // flip the next bit from Lengyel since we're right-handed
+        //matrix[2][2] = c.z; 
+        matrix[2][2] = -c.z; 
+        matrix[2][3] = c.w;        
+
     }
 
 
