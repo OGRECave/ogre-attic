5518688720fa56d66464eb4576bb82c7ba263c95
miathan6 <miathan6@0896a727-e8d4-4d2b-8422-44d28760243c>
2004-11-11 19:57:33 +0000

    Print value of hardware render-to-texture capability

--- trunk/OgreMain/src/OgreRenderSystemCapabilities.cpp
+++ trunk/OgreMain/src/OgreRenderSystemCapabilities.cpp
@@ -126,6 +126,9 @@ namespace Ogre {
         pLog->logMessage(
             " * Infinite far plane projection: " 
             + StringConverter::toString(hasCapability(RSC_INFINITE_FAR_PLANE), true));
+		pLog->logMessage(
+            " * Hardware render-to-texture: " 
+            + StringConverter::toString(hasCapability(RSC_HWRENDER_TO_TEXTURE), true));
         pLog->logMessage(
             " * Floating point textures: " 
             + StringConverter::toString(hasCapability(RSC_TEXTURE_FLOAT), true));
