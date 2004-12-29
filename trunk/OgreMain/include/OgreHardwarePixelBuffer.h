905840767e3053c92548978d21034b971e8b30e6
sinbad <sinbad@0896a727-e8d4-4d2b-8422-44d28760243c>
2004-12-29 19:55:36 +0000

    Added new hardware pixel buffer classes to VC7 Make full-buffer HardwareBuffer::lock virtual and removed override in pixel buffer

--- trunk/OgreMain/include/OgreHardwarePixelBuffer.h
+++ trunk/OgreMain/include/OgreHardwarePixelBuffer.h
@@ -77,11 +77,6 @@ namespace Ogre {
 		/// @copydoc HardwareBuffer::lock
         virtual void* lock(size_t offset, size_t length, LockOptions options);
 
-		void* lock(LockOptions options)
-        {
-        	return this->lock(0, mSizeInBytes, options);
-        }
-
 		/** Get the current locked region. This is the same value as returned
 		    by lock(const Image::Box, LockOptions)
 		    @returns PixelBox containing the locked region
