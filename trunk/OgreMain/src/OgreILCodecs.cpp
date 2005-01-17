e2f6c8a85dae91e5c364e970ecb9a0fda924ed6a
miathan6 <miathan6@0896a727-e8d4-4d2b-8422-44d28760243c>
2005-01-17 16:17:07 +0000

    - Fix devil formats listing - Implement our own ilFormatFromExt

--- trunk/OgreMain/src/OgreILCodecs.cpp
+++ trunk/OgreMain/src/OgreILCodecs.cpp
@@ -38,6 +38,117 @@ http://www.gnu.org/copyleft/lesser.txt.
 namespace Ogre {
     std::list<ILImageCodec*> ILCodecs::codeclist;
 
+	// Return IL type for a certain extension
+	ILenum ogreIlTypeFromExt(const String &ext)
+	{
+#ifdef IL_TGA
+		if (ext=="tga" || ext=="vda" ||
+			ext=="icb" || ext=="vst")
+			return IL_TGA;
+#endif
+#ifdef IL_JPG
+		if (ext=="jpg" || ext=="jpe" || ext=="jpeg")
+			return IL_JPG;
+#endif
+#ifdef IL_DDS
+		if (ext=="dds")
+			return IL_DDS;
+#endif
+#ifdef IL_PNG
+		if (ext=="png")
+			return IL_PNG;
+#endif
+#ifdef IL_BMP
+		if (ext=="bmp" || ext=="dib")
+			return IL_BMP;
+#endif
+#ifdef IL_GIF
+		if (ext=="gif")
+			return IL_GIF;
+#endif
+#ifdef IL_CUT
+		if (ext=="cut")
+			return IL_CUT;
+#endif
+#ifdef IL_HDR
+		if (ext=="hdr")
+			return IL_HDR;
+#endif
+#ifdef IL_ICO
+		if (ext=="ico" || ext=="cur")
+			return IL_ICO;
+#endif
+#ifdef IL_JNG
+		if (ext=="jng")
+			return IL_JNG;
+#endif
+#ifdef IL_LIF
+		if (ext=="lif")
+			return IL_LIF;
+#endif
+#ifdef IL_MDL
+		if (ext=="mdl")
+			return IL_MDL;
+#endif
+#ifdef IL_MNG
+		if (ext=="mng" || ext=="jng")
+			return IL_MNG;
+#endif
+#ifdef IL_PCD
+		if (ext=="pcd")
+			return IL_PCD;
+#endif
+#ifdef IL_PCX
+		if (ext=="pcx")
+			return IL_PCX;
+#endif
+#ifdef IL_PIC
+		if (ext=="pic")
+			return IL_PIC;
+#endif
+#ifdef IL_PIX
+		if (ext=="pix")
+			return IL_PIX;
+#endif
+#ifdef IL_PNM
+		if (ext=="pbm" || ext=="pgm" ||
+			ext=="pnm" || ext=="ppm")
+			return IL_PNM;
+#endif
+#ifdef IL_PSD
+		if (ext=="psd" || ext=="pdd")
+			return IL_PSD;
+#endif
+#ifdef IL_PSP
+		if (ext=="psp")
+			return IL_PSP;
+#endif
+#ifdef IL_PXR
+		if (ext=="pxr")
+			return IL_PXR;
+#endif
+#ifdef IL_SGI
+		if (ext=="sgi" || ext=="bw" ||
+			ext=="rgb" || ext=="rgba")
+			return IL_SGI;
+#endif
+#ifdef IL_TIF
+		if (ext=="tif" || ext=="tiff")
+			return IL_TIF;
+#endif
+#ifdef IL_WAL
+		if (ext=="wal")
+			return IL_WAL;
+#endif
+#ifdef IL_XPM
+		if (ext=="xpm")
+			return IL_XPM;
+#endif
+	
+		return IL_TYPE_UNKNOWN;
+	}
+
+	
     void ILCodecs::registerCodecs(void) {
 		const char *il_version = ilGetString ( IL_VERSION_NUM );
 		LogManager::getSingleton().logMessage(
@@ -53,19 +164,21 @@ namespace Ogre {
 #if 0
 			String fileName = "dummy." + str;
 			// DevIL doesn't export this under windows -- how weird
-			int ilType = ilTypeFromExt(const_cast<char*>(fileName.c_str()));
+			int ilType = ilTypeFromext(const_cast<char*>(fileName.c_str()));
 #endif
-			int ilType = IL_TYPE_UNKNOWN;
+			int ilType = ogreIlTypeFromExt(str);
             ILImageCodec *codec = new ILImageCodec(str, ilType);
             Codec::registerCodec(codec);
             codeclist.push_back(codec);
 			//all += str+String("(")+StringConverter::toString(ilType)+String(") ");
+			all += str+String(" ");
         }
 		// Raw format, missing in image formats string
 		ILImageCodec *cod = new ILImageCodec("raw", IL_RAW);
 		Codec::registerCodec(cod);
 		codeclist.push_back(cod);
 		//all += String("raw")+"("+StringConverter::toString(IL_RAW)+String(") ");
+		all += String("raw ");
 		LogManager::getSingleton().logMessage(
          LML_NORMAL,
             "DevIL image formats: " + all);
