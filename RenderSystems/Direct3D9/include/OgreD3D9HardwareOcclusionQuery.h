fc0b68f4a5fa2fdae3b63f0417b943c1460cc723
sinbad <sinbad>
2005-03-26 15:34:25 +0000

    [ 1092846 ] Hardware Occlusion fixes from leedgitar

--- RenderSystems/Direct3D9/include/OgreD3D9HardwareOcclusionQuery.h
+++ RenderSystems/Direct3D9/include/OgreD3D9HardwareOcclusionQuery.h
@@ -1,7 +1,7 @@
 /*
 -----------------------------------------------------------------------------
 This source file is part of OGRE
-    (Object-oriented Graphics Rendering Engine)
+(Object-oriented Graphics Rendering Engine)
 For the latest info, see http://ogre.sourceforge.net/
 
 Copyright (c) 2000-2005 The OGRE Team
@@ -32,80 +32,76 @@ http://www.gnu.org/copyleft/lesser.txt.
 
 namespace Ogre {
 
+	// If you use multiple rendering passes you can test only the first pass and all other passes don't have to be rendered 
+	// if the first pass resultet has too few pixels visible.
 
-
-// If you use multiple rendering passes you can test only the first pass and all other passes don't have to be rendered 
-// if the first pass resultet has too few pixels visable.
-
-// Be sure to render all occlluder first and whats out so the RenderQue don't switch places on 
-// the occluding objects and the tested objects because it thinks it's more effective..
-
-/**
-  * This is a class that is the DirectX9 implementation of 
-  * hardware occlusion testing.
-  *
-  * @author Lee Sandberg, email lee@abcmedia.se
-  */
-class D3D9HardwareOcclusionQuery : public HardwareOcclusionQuery
-{
-//----------------------------------------------------------------------
-// Public methods
-//--
-public:
+	// Be sure to render all occlluder first and whats out so the RenderQue don't switch places on 
+	// the occluding objects and the tested objects because it thinks it's more effective..
 
 	/**
-	  * Default object constructor
-	  * 
-	  */
-	D3D9HardwareOcclusionQuery( IDirect3DDevice9* pD3DDevice );
+	* This is a class that is the DirectX9 implementation of 
+	* hardware occlusion testing.
+	*
+	* @author Lee Sandberg, email lee@abcmedia.se
+	*
+	* Updated on 12/7/2004 by Chris McGuirk
+	*/
+	class D3D9HardwareOcclusionQuery : public HardwareOcclusionQuery
+	{
+		//----------------------------------------------------------------------
+		// Public methods
+		//--
+	public:
 
-	/**
-	  * Object destructor
-	  */
-	~D3D9HardwareOcclusionQuery();
+		/**
+		* Default object constructor
+		* 
+		*/
+		D3D9HardwareOcclusionQuery( IDirect3DDevice9* pD3DDevice );
 
-	//------------------------------------------------------------------
-	// Occlusion query functions (see base class documentation for this)
-	//--
+		/**
+		* Object destructor
+		*/
+		~D3D9HardwareOcclusionQuery();
 
-	void beginOcclusionQuery();	
-	void endOcclusionQuery();
-	bool pullOcclusionQuery( unsigned int* NumOfFragments);
-	bool pullOcclusionQuery( unsigned int* NumOfFragments, const HW_OCCLUSIONQUERY flag  );
-	unsigned int getLastQuerysPixelcount() { return m_uintPixelCount; }
+		//------------------------------------------------------------------
+		// Occlusion query functions (see base class documentation for this)
+		//--
 
-	// This functions are optional, it's a simple filter that simply skipps some hardware occlusion tests on visable objects only
-	// It's easy to use if you don't have to keep track on which objects are visable (can be skipped) and what objects arn't visable..
-	// (None visable objects and object you introduce for the first time have allways to be tested allthough the cheepest possible 
-	// LOD (Level Of Detail) mesh and material wize). 
+		void beginOcclusionQuery();	
+		void endOcclusionQuery();
+		bool pullOcclusionQuery( unsigned int* NumOfFragments, const HW_OCCLUSIONQUERY flag = HWOCCLUSIONQUERY_FLUSH);
+		unsigned int getLastQuerysPixelcount() { return mPixelCount; }
 
-	/**
-	  *   
-	  * Remarks This function allows you to set how often the hardware occlusion really are sent to the driver
-	  * if you set it to 0 every hw occlusion test is acctually made. If you set it to 1 only the half of your queries are sent 
-	  * for all visable objects. 2 will result in 25% of all queries to acctualy be sent. 
-	  * New and none visable objects will be tested all the time.
-	  * This functionality is here because this class can keep track on visable and none visable objects for you.
-	  * Once you you set the SkipRate for any hardware occlusion instance it effects all others.
-	  */
+		// These functions are optional, it's a simple filter that simply skips some hardware occlusion tests on visible objects only
+		// It's easy to use if you don't have to keep track of which objects are visible (can be skipped) and what objects arn't visible..
+		// (None visible objects and object you introduce for the first time have always to be tested although the cheapest possible 
+		// LOD (Level Of Detail) mesh and material-wise). 
 
+		/**
+		*   
+		* Remarks This function allows you to set how often the hardware occlusion query is sent to the driver
+		* if you set it to 0 every hw occlusion test is acctually made. If you set it to 1 only the half of your queries are sent 
+		* 2 will result in 25% of all queries to acctualy be sent. 
+		* This functionality is here because this class can keep track on visible and none visible objects for you.
+		* Once you you set the SkipRate for any hardware occlusion instance it effects all others.
+		*/
 
-	void setSkipRate( int skip ) { m_Skip = skip; }			// Using 2 only 50 % of the tests are actully made and 3 results in only 33% of the tests. So on.
-	int	 getSkipRate() { return m_Skip; } 
+		void setSkipRate( int skip ) { mSkipInterval = skip; }			// Using 2 only 50 % of the tests are actully made and 3 results in only 33% of the tests. So on.
+		int	 getSkipRate() { return mSkipInterval; } 
 
-//----------------------------------------------------------------------
-// Protected members
-//--
-protected:
+		//----------------------------------------------------------------------
+		// Protected members
+		//--
+	protected:
 
-	unsigned int		m_uintPixelCount;		
-	IDirect3DQuery9*	m_pD3DQuery;
-	IDirect3DDevice9*   m_pD3DDevice;       // d3d device
-	bool				m_bOcclusionQuery;
-	int					m_SkipCounter;		// m_SkipConter =  m_SkipConter % m_Skip; if ( m_SkipConter == 0 && m_uintPixelCount !=0 ) TestHWOcclusion else just return 
-	static int			m_Skip;				// This is shared by all instancies
-	bool				m_bHWOcclusionSupport;
-};
+		unsigned int		mPixelCount;		
+		IDirect3DQuery9*	mpQuery;
+		IDirect3DDevice9*   mpDevice;
+		int					mSkipCounter;
+		int					mSkipInterval;
+		bool				mHasOcclusionSupport;
+	};
 
 
 }
