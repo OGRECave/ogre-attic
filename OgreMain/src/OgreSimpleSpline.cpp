2cc88f510e05c91d69aeb85bae27ca6032548e3a
sinbad <sinbad>
2005-07-17 17:17:21 +0000

    [ 1235745 ] Off-by-one bug in SimpleSpline & RotationSpline

--- OgreMain/src/OgreSimpleSpline.cpp
+++ OgreMain/src/OgreSimpleSpline.cpp
@@ -77,7 +77,7 @@ namespace Ogre {
 
         
         // Work out which segment this is in
-        Real fSeg = t * mPoints.size();
+        Real fSeg = t * (mPoints.size() - 1);
         unsigned int segIdx = (unsigned int)fSeg;
         // Apportion t 
         t = fSeg - segIdx;
