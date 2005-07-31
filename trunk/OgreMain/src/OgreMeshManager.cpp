b1f872bb146ee26258cf972768555aae87293499
miathan6 <miathan6@0896a727-e8d4-4d2b-8422-44d28760243c>
2005-07-31 10:32:25 +0000

    Normalise generated normals for fixed plane

--- trunk/OgreMain/src/OgreMeshManager.cpp
+++ trunk/OgreMain/src/OgreMeshManager.cpp
@@ -658,11 +658,11 @@ namespace Ogre
                 vec.z = (-sin((1-dist) * (PI/2)) * params.curvature) + params.curvature;
 
                 // Transform by orientation and distance
-                vec = xform * vec;
+                Vector3 pos = xform * vec;
                 // Assign to geometry
-                *pFloat++ = vec.x;
-                *pFloat++ = vec.y;
-                *pFloat++ = vec.z;
+                *pFloat++ = pos.x;
+                *pFloat++ = pos.y;
+                *pFloat++ = pos.z;
 
                 // Record bounds
                 if (first)
@@ -685,9 +685,10 @@ namespace Ogre
                     //   so I'm not going to mess with it for now. 
 
                     // Default normal is along unit Z
-                    vec = Vector3::UNIT_Z;
+                    //vec = Vector3::UNIT_Z;
                     // Rotate
                     vec = rot * vec;
+					vec.normalise();
 
                     *pFloat++ = vec.x;
                     *pFloat++ = vec.y;
