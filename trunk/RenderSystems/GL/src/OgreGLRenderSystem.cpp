6ba61c73ac537067ad6431b8112a7c263e9ce225
sinbad <sinbad@0896a727-e8d4-4d2b-8422-44d28760243c>
2005-08-28 21:10:07 +0000

    Azathoth merged into HEAD

--- trunk/RenderSystems/GL/src/OgreGLRenderSystem.cpp
+++ trunk/RenderSystems/GL/src/OgreGLRenderSystem.cpp
@@ -898,7 +898,8 @@ namespace Ogre {
 
 			if(!tex.isNull())
 				glBindTexture( mTextureTypes[stage], tex->getGLID() );
-
+			else
+				glBindTexture( mTextureTypes[stage], static_cast<GLTextureManager*>(mTextureManager)->getWarningTextureID() );
         }
         else
         {
@@ -2500,6 +2501,42 @@ namespace Ogre {
         }
     }
     //---------------------------------------------------------------------
+    void GLRenderSystem::_switchContext(GLContext *context)
+    {
+        // Unbind GPU programs and rebind to new context later, because
+        // scene manager treat render system as ONE 'context' ONLY, and it
+        // cached the GPU programs using state.
+        if (mCurrentVertexProgram)
+            mCurrentVertexProgram->unbindProgram();
+        if (mCurrentFragmentProgram)
+            mCurrentFragmentProgram->unbindProgram();
+
+        // It's ready to switching
+        mCurrentContext->endCurrent();
+        mCurrentContext = context;
+        mCurrentContext->setCurrent();
+        
+        // Check if the context has already done one-time initialisation
+        if(!mCurrentContext->getInitialized()) 
+        {
+            _oneTimeContextInitialization();
+            mCurrentContext->setInitialized();
+        }
+
+        // Rebind GPU programs to new context
+        if (mCurrentVertexProgram)
+            mCurrentVertexProgram->bindProgram();
+        if (mCurrentFragmentProgram)
+            mCurrentFragmentProgram->bindProgram();
+
+        // Must reset depth/colour write mask to according with user desired, otherwise,
+        // clearFrameBuffer would be wrong because the value we are recorded may be
+        // difference with the really state stored in GL context.
+        glDepthMask(mDepthWrite);
+        glColorMask(mColourWrite[0], mColourWrite[1], mColourWrite[2], mColourWrite[3]);
+
+    }
+    //---------------------------------------------------------------------
     void GLRenderSystem::_setRenderTarget(RenderTarget *target)
     {
         // Unbind frame buffer object
@@ -2513,16 +2550,7 @@ namespace Ogre {
         target->getCustomAttribute("GLCONTEXT", &newContext);
         if(newContext && mCurrentContext != newContext) 
         {
-            // This rendertarget has a registered context, activate it
-            mCurrentContext->endCurrent();
-            mCurrentContext = newContext;
-            mCurrentContext->setCurrent();
-            
-            // Check if the context has already done one-time initialisation
-            if(!mCurrentContext->getInitialized()) {
-               _oneTimeContextInitialization();
-               mCurrentContext->setInitialized();
-            }
+            _switchContext(newContext);
         }
         
         // Bind frame buffer object
@@ -2536,9 +2564,7 @@ namespace Ogre {
             // remains active. When this is the main context being unregistered,
             // we set the main context to 0.
             if(mCurrentContext != mMainContext) {
-                mCurrentContext->endCurrent();
-                mCurrentContext = mMainContext;
-                mCurrentContext->setCurrent();
+                _switchContext(mMainContext);
             } else {
                 /// No contexts remain
                 mCurrentContext->endCurrent();
--- trunk/RenderSystems/GL/src/OgreGLTextureManager.cpp
+++ trunk/RenderSystems/GL/src/OgreGLTextureManager.cpp
@@ -28,16 +28,20 @@ http://www.gnu.org/copyleft/lesser.txt.
 namespace Ogre {
     //-----------------------------------------------------------------------------
     GLTextureManager::GLTextureManager(GLSupport& support)
-        : TextureManager(), mGLSupport(support)
+        : TextureManager(), mGLSupport(support), mWarningTextureID(0)
     {
         // register with group manager
         ResourceGroupManager::getSingleton()._registerResourceManager(mResourceType, this);
+
+		createWarningTexture();
     }
     //-----------------------------------------------------------------------------
     GLTextureManager::~GLTextureManager()
     {
         // unregister with group manager
         ResourceGroupManager::getSingleton()._unregisterResourceManager(mResourceType);
+		// Delete warning texture
+		glDeleteTextures(1, &mWarningTextureID);
     }
     //-----------------------------------------------------------------------------
     Resource* GLTextureManager::createImpl(const String& name, ResourceHandle handle, 
@@ -46,5 +50,30 @@ namespace Ogre {
     {
         return new GLTexture(this, name, handle, group, isManual, loader, mGLSupport);
     }
+
+	//-----------------------------------------------------------------------------
+	void GLTextureManager::createWarningTexture()
+	{
+		// Generate warning texture
+		size_t width = 8;
+		size_t height = 8;
+		uint32 *data = new uint32[width*height];		// 0xXXRRGGBB
+		// Yellow/black stripes
+		for(size_t y=0; y<height; ++y)
+		{
+			for(size_t x=0; x<width; ++x)
+			{
+				data[y*width+x] = (((x+y)%8)<4)?0x000000:0xFFFF00;
+			}
+		}
+		// Create GL resource
+        glGenTextures(1, &mWarningTextureID);
+		glBindTexture(GL_TEXTURE_2D, mWarningTextureID);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 0);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB8, width, height, 0, GL_BGRA, GL_UNSIGNED_INT_8_8_8_8_REV, (void*)data);
+		// Free memory
+		delete [] data;
+	}
+
 
 }
