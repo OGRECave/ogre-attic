3ee942f38acfe294a1e76e70ad3acf7d18a1991a
sinbad <sinbad>
2005-11-11 23:01:26 +0000

    Added documentation & shortcuts to XSI v5 exporter setup Changed to featuretree UI to skip typical/complete/custom install selection Renamed image files so they'd work if short names required Updated changelog

--- Tools/XSIExport/setup/build.bat
+++ Tools/XSIExport/setup/build.bat
@@ -1,2 +1,2 @@
 candle xsi5install.wxs
-light -out OgreXSIExporter5.msi xsi5install.wixobj %WIX_ROOT%\ui\wixui_mondo.wixlib
+light -out OgreXSIExporter5.msi xsi5install.wixobj %WIX_ROOT%\ui\wixui_featuretree.wixlib
