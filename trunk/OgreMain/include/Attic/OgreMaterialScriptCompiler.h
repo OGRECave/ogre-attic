629a7472695ab752e9966ad50675f2d909f90f64
nfz <nfz@0896a727-e8d4-4d2b-8422-44d28760243c>
2005-12-15 04:49:32 +0000

    added Point size, Shading, got rid of deprecated non terminal token enums

--- trunk/OgreMain/include/Attic/OgreMaterialScriptCompiler.h
+++ trunk/OgreMain/include/Attic/OgreMaterialScriptCompiler.h
@@ -64,42 +64,13 @@ namespace Ogre {
             ID_COLOUR_WRITE, ID_CULL_HARDWARE, ID_CLOCKWISE, ID_ANTICLOCKWISE, ID_CULL_NONE,
             ID_CULL_SOFTWARE, ID_CULL_BACK, ID_CULL_FRONT,
             ID_SHADING, ID_FLAT, ID_GOURAUD, ID_PHONG,
-            ID_LIGHTING, ID_MAX_LIGHTS, ID_FOG_OVERRIDE,
+            ID_LIGHTING, ID_MAX_LIGHTS, ID_FOG_OVERRIDE, ID_POINT_SIZE,
+
             ID_TEXTURE_UNIT,
 
             // general
             ID_ON, ID_OFF, ID_TRUE, ID_FALSE
         };
-
-        /* deprecated: rules are now written in BNF text format and will be compiled from string into the rule base
-
-
-    		// non-terminal tokens section
-            mid_SCRIPT, mid_SCRIPT_OPTIONS, mid_SCRIPT_PROPERTIES, mid_MATERIAL_DEF, mid_VERTEX_PROGRAM_DEF,
-            mid_FRAGMENT_PROGRAM_DEF, mid_MATERIAL_CLONE, mid_MATERIAL_OPTIONS, mid_MATERIAL_PROPERTIES,
-
-            mid_TECHNIQUE_DEF, mid_SET_TEXTURE_ALIAS_DEF, mid_LOD_DISTANCES_DEF, mid_RECEIVE_SHADOWS_DEF,
-            mid_TRANSPARENCY_CASTS_SHADOWS_DEF, mid_TECHNIQUE_OPTIONS, mid_TECHNIQUE_PROPERTIES,
-
-            mid_PASS_DEF, mid_PASS_OPTIONS, mid_LOD_INDEX_DEF, mid_PASS_PROPERTIES, mid_AMBIENT_DEF, mid_DIFFUSE_DEF, 
-			mid_EMISSIVE_DEF, mid_SPECULAR_DEF, mid_SPECULAR_PARAMS, mid_VERTEXCOLOUR_DEF, mid_COLOUR_PARAM_DEF,
-            mid_SCENE_BLEND_DEF, mid_SIMPLE_BLEND, mid_USER_BLEND, mid_BLEND_FACTOR,
-            mid_DEPTH_CHECK_DEF, mid_DEPTH_WRITE_DEF, mid_DEPTH_FUNC_DEF,
-            mid_ALPHA_REJECTION_DEF, mid_COMPARE_FUNC_DEF, mid_FOG_PARAMETERS_DEF,
-            mid_COLOUR_WRITE_DEF, mid_CULL_HARDWARE_DEF, mid_CULL_SOFTWARE_DEF, 
-            mid_LIGHTING_DEF, mid_FOG_OVERRIDE_DEF, mid_SHADING_DEF, mid_DEPTH_BIAS_DEF, mid_TEXTURE_UNIT_DEF, 
-            mid_TUS_OPTIONS, mid_TUS_PROPERTIES, mid_VERTEX_PROGRAM_REF_DEF, mid_SHADOW_CASTER_VERTEX_PROGRAM_REF_DEF,
-            mid_SHADOW_RECEIVER_VERTEX_PROGRAM_REF_DEF, mid_FRAGMENT_PROGRAM_REF_DEF,
-            mid_MAX_LIGHTS_DEF, mid_ITERATION_DEF, 
-
-            mid_ON_OFF, mid_TRUE_FALSE_DEF,
-            mid_LABEL, mid_QUOTED_LABEL, mid_UNQUOTED_LABEL, mid_ALPHANUMERIC_SPACE,
-            mid_ALPHANUMERIC, mid_CHARACTER, mid_NUMBER,
-
-            // system token base
-            mid_SYSTEM
-        };
-        */
 
         /** Enum to identify material sections. */
         enum MaterialScriptSection
@@ -208,6 +179,8 @@ namespace Ogre {
         void parseCullSoftware(void);
         void parseLighting(void);
         void parseMaxLights(void);
+        void parseShading(void);
+        void parsePointSize(void);
 
         void parseTextureUnit(void);
         void parseTextureCustomParameter(void);
