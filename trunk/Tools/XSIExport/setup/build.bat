73a5e942ab97785c27ac4c6ca087ed9f0eb5491f
sinbad <sinbad@0896a727-e8d4-4d2b-8422-44d28760243c>
2006-01-02 18:28:47 +0000

    Build XSI exporter setup using Wix for v4.x as well as v5

--- trunk/Tools/XSIExport/setup/build.bat
+++ trunk/Tools/XSIExport/setup/build.bat
@@ -1,2 +1,21 @@
+@echo off
+if "%1" == "4" goto xsi4
+if "%1" =="5" goto xsi5
+:usage
+echo build [version]
+goto end
+
+:xsi5
 candle xsi5install.wxs
 light -out OgreXSIExporter5.msi xsi5install.wixobj %WIX_ROOT%\ui\wixui_featuretree.wixlib
+goto end
+
+:xsi4:
+candle xsi4install.wxs
+light -out OgreXSIExporter4.msi xsi4install.wixobj %WIX_ROOT%\ui\wixui_featuretree.wixlib
+goto end
+
+
+
+:end
+
