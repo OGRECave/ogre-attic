9ab1f4c1d80d524873da64b3d6c35279f28d504c
genva <genva>
2006-01-12 05:39:34 +0000

    Bugfix - Avoid multi-results of same object from OctreePlaneBoundedVolumeListSceneQuery

--- PlugIns/OctreeSceneManager/src/OgreOctreeSceneQuery.cpp
+++ PlugIns/OctreeSceneManager/src/OgreOctreeSceneQuery.cpp
@@ -279,6 +279,8 @@ OctreePlaneBoundedVolumeListSceneQuery::~OctreePlaneBoundedVolumeListSceneQuery(
 //---------------------------------------------------------------------
 void OctreePlaneBoundedVolumeListSceneQuery::execute(SceneQueryListener* listener)
 {
+    std::set<SceneNode*> checkedSceneNodes;
+
     PlaneBoundedVolumeList::iterator pi, piend;
     piend = mVolumes.end();
     for (pi = mVolumes.begin(); pi != piend; ++pi)
@@ -288,9 +290,13 @@ void OctreePlaneBoundedVolumeListSceneQuery::execute(SceneQueryListener* listene
         static_cast<OctreeSceneManager*>( mParentSceneMgr ) -> findNodesIn( *pi, list, 0 );
 
         //grab all moveables from the node that intersect...
-        std::list < SceneNode * >::iterator it = list.begin();
-        while( it != list.end() )
+        std::list < SceneNode * >::iterator it, itend;
+        itend = list.end();
+        for (it = list.begin(); it != itend; ++it)
         {
+            // avoid double-check same scene node
+            if (!checkedSceneNodes.insert(*it).second)
+                continue;
             SceneNode::ObjectIterator oit = (*it) -> getAttachedObjectIterator();
             while( oit.hasMoreElements() )
             {
@@ -317,7 +323,6 @@ void OctreePlaneBoundedVolumeListSceneQuery::execute(SceneQueryListener* listene
 					}
                 }
             }
-            ++it;
         }
     }//for
 }
