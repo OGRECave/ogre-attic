8118672e261c0a94f4926be29e477274d460c895
nfz <nfz>
2006-02-02 23:28:29 +0000

    fixed absence of Unquoted_Label in grammer

--- OgreMain/src/OgreCompositorScriptCompiler.cpp
+++ OgreMain/src/OgreCompositorScriptCompiler.cpp
@@ -62,6 +62,7 @@ namespace Ogre {
 		"<On_Off> ::= 'on' | 'off' \n"
 		"<Label> ::= <Quoted_Label> | <Unquoted_Label> \n"
 		"<Quoted_Label> ::= '\"' <Character> {<Alphanumeric_Space>} '\"' \n"
+        "<Unquoted_Label> ::= <Character> {<Alphanumeric>} \n"
 		"<Alphanumeric_Space> ::= <Alphanumeric> | ' ' \n"
 		"<Alphanumeric> ::= <Character> | <Number> \n"
 		"<Character> ::= (abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ$#%!_*&\\/) \n"
