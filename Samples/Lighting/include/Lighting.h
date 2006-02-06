625dd23ddd0b28939a05da2aa8ab5f6352d22941
sinbad <sinbad>
2006-02-06 00:17:43 +0000

    Lighting demo quick update with ribbons

--- Samples/Lighting/include/Lighting.h
+++ Samples/Lighting/include/Lighting.h
@@ -76,6 +76,8 @@ public:
 // Some global node data
 SceneNode* mRedYellowLightsNode;
 SceneNode* mGreenBlueLightsNode;
+std::vector<AnimationState*> mAnimStateList;
+
 
 // Listener class for frame updates
 class LightingListener : public ExampleFrameListener
@@ -89,8 +91,13 @@ public:
 
     bool frameStarted(const FrameEvent& evt)
     {
-        mRedYellowLightsNode->yaw(Degree(evt.timeSinceLastFrame * 10));
-        mGreenBlueLightsNode->pitch(Degree(evt.timeSinceLastFrame * 20));
+
+		std::vector<AnimationState*>::iterator animi;
+		for (animi = mAnimStateList.begin(); animi != mAnimStateList.end(); ++animi)
+		{
+			(*animi)->addTime(evt.timeSinceLastFrame);
+		}
+
         // Call superclass
         return ExampleFrameListener::frameStarted(evt);
     }
@@ -151,6 +158,7 @@ protected:
 		// Attach the head to the scene
 		mSceneMgr->getRootSceneNode()->attachObject(head);
 
+		/*
 		// Create nodes for the lights to be rotated with
         mRedYellowLightsNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
         mGreenBlueLightsNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
@@ -243,7 +251,116 @@ protected:
 		mYellowLightController = mControllerManager->createController(mControllerManager->getFrameTimeSource(), mYellowLightFlasher, mYellowLightControllerFunc);
 		mGreenLightController = mControllerManager->createController(mControllerManager->getFrameTimeSource(), mGreenLightFlasher, mGreenLightControllerFunc);
 
+		*/
+
+		setupTrailLights();
+
     }
+
+	void setupTrailLights(void)
+	{
+		mSceneMgr->setAmbientLight(ColourValue(0.5, 0.5, 0.5));
+		Vector3 dir(-1, -1, 0.5);
+		dir.normalise();
+		Light* l = mSceneMgr->createLight("light1");
+		l->setType(Light::LT_DIRECTIONAL);
+		l->setDirection(dir);
+
+		NameValuePairList pairList;
+		pairList["numberOfChains"] = "2";
+		pairList["maxElements"] = "80";
+		RibbonTrail* trail = static_cast<RibbonTrail*>(
+			mSceneMgr->createMovableObject("1", "RibbonTrail", &pairList));
+		trail->setMaterialName("Examples/LightRibbonTrail");
+		trail->setTrailLength(400);
+
+
+		mSceneMgr->getRootSceneNode()->createChildSceneNode()->attachObject(trail);
+
+		// Create 3 nodes for trail to follow
+		SceneNode* animNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+		animNode->setPosition(50,30,0);
+		Animation* anim = mSceneMgr->createAnimation("an1", 14);
+		anim->setInterpolationMode(Animation::IM_SPLINE);
+		NodeAnimationTrack* track = anim->createNodeTrack(1, animNode);
+		TransformKeyFrame* kf = track->createNodeKeyFrame(0);
+		kf->setTranslate(Vector3(50,30,0));
+		kf = track->createNodeKeyFrame(2);
+		kf->setTranslate(Vector3(100, -30, 0));
+		kf = track->createNodeKeyFrame(4);
+		kf->setTranslate(Vector3(120, -100, 150));
+		kf = track->createNodeKeyFrame(6);
+		kf->setTranslate(Vector3(30, -100, 50));
+		kf = track->createNodeKeyFrame(8);
+		kf->setTranslate(Vector3(-50, 30, -50));
+		kf = track->createNodeKeyFrame(10);
+		kf->setTranslate(Vector3(-150, -20, -100));
+		kf = track->createNodeKeyFrame(12);
+		kf->setTranslate(Vector3(-50, -30, 0));
+		kf = track->createNodeKeyFrame(14);
+		kf->setTranslate(Vector3(50,30,0));
+
+		AnimationState* animState = mSceneMgr->createAnimationState("an1");
+		animState->setEnabled(true);
+		mAnimStateList.push_back(animState);
+
+		trail->addNode(animNode);
+		trail->setInitialColour(0, 1.0, 0.8, 0);
+		trail->setColourChange(0, 0.5, 0.5, 0.5, 0.5);
+		trail->setInitialWidth(0, 5);
+
+		// Add light
+		Light* l2 = mSceneMgr->createLight("l2");
+		l2->setDiffuseColour(trail->getInitialColour(0));
+		animNode->attachObject(l2);
+
+		// Add billboard
+		BillboardSet* bbs = mSceneMgr->createBillboardSet("bb", 1);
+		bbs->createBillboard(Vector3::ZERO, trail->getInitialColour(0));
+		bbs->setMaterialName("Examples/Flare");
+		animNode->attachObject(bbs);
+
+		animNode = mSceneMgr->getRootSceneNode()->createChildSceneNode();
+		animNode->setPosition(-50,100,0);
+		anim = mSceneMgr->createAnimation("an2", 10);
+		anim->setInterpolationMode(Animation::IM_SPLINE);
+		track = anim->createNodeTrack(1, animNode);
+		kf = track->createNodeKeyFrame(0);
+		kf->setTranslate(Vector3(-50,100,0));
+		kf = track->createNodeKeyFrame(2);
+		kf->setTranslate(Vector3(-100, 150, -30));
+		kf = track->createNodeKeyFrame(4);
+		kf->setTranslate(Vector3(-200, 0, 40));
+		kf = track->createNodeKeyFrame(6);
+		kf->setTranslate(Vector3(0, -150, 70));
+		kf = track->createNodeKeyFrame(8);
+		kf->setTranslate(Vector3(50, 0, 30));
+		kf = track->createNodeKeyFrame(10);
+		kf->setTranslate(Vector3(-50,100,0));
+
+		animState = mSceneMgr->createAnimationState("an2");
+		animState->setEnabled(true);
+		mAnimStateList.push_back(animState);
+
+		trail->addNode(animNode);
+		trail->setInitialColour(1, 0.0, 1.0, 0.4);
+		trail->setColourChange(1, 0.5, 0.5, 0.5, 0.5);
+		trail->setInitialWidth(1, 5);
+
+
+		// Add light
+		l2 = mSceneMgr->createLight("l3");
+		l2->setDiffuseColour(trail->getInitialColour(1));
+		animNode->attachObject(l2);
+
+		// Add billboard
+		bbs = mSceneMgr->createBillboardSet("bb2", 1);
+		bbs->createBillboard(Vector3::ZERO, trail->getInitialColour(1));
+		bbs->setMaterialName("Examples/Flare");
+		animNode->attachObject(bbs);
+
+
+	}
 
 	void createFrameListener(void)
     {
