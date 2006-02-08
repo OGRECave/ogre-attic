1b5461751c36091b6af4d08c9d99dad69906e667
genva <genva@0896a727-e8d4-4d2b-8422-44d28760243c>
2006-02-08 07:08:35 +0000

    All demo compositors work fine now.

--- trunk/Samples/Media/materials/programs/Copyback_ps11.hlsl
+++
@@ -1,7 +0,0 @@
-sampler Temp;
-
-float4 main(float2 texCoord: TEXCOORD0) : COLOR 
-{
-   return tex2D(Temp, texCoord);
-}
-
--- trunk/Samples/Media/materials/programs/MotionBlur_ps11.hlsl
+++
@@ -1,5 +0,0 @@
-sampler Sum: register(s0);
-float4 main(float2 texCoord: TEXCOORD0) : COLOR {
-   return tex2D(Sum, texCoord);
-}
-
