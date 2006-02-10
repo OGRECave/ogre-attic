37752b8108703875a64e7cf72b65f7688d5a1ba3
cos_xavier <cos_xavier>
2006-02-10 02:12:28 +0000

    - Changed copyright function in OgreMaxExport.cpp - Aded LGPL notice to all of the source files (including the .rc file)

--- Tools/3dsmaxExport/OgreExport/src/OgreMaxExport.cpp
+++ Tools/3dsmaxExport/OgreExport/src/OgreMaxExport.cpp
@@ -1,3 +1,28 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE 
+(Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+
+Copyright (c) 2000-2005 The OGRE Team
+Also see acknowledgements in Readme.html
+
+This program is free software; you can redistribute it and/or modify it under 
+the terms of the GNU Lesser General Public License as published by the Free Software 
+Foundation; either version 2 of the License, or (at your option) any later 
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT 
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS 
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with 
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple 
+Place - Suite 330, Boston, MA 02111-1307, USA, or go to 
+http://www.gnu.org/copyleft/lesser.txt.
+-----------------------------------------------------------------------------
+*/
+
 #include "windows.h"
 #include "max.h"
 #include "plugapi.h"
@@ -179,11 +204,11 @@ const TCHAR * OgreMaxExport::ShortDesc() {
 }
 
 const TCHAR * OgreMaxExport::AuthorName() { 
-	return _T("Gregory Junker");
+	return _T("Gregory 'Xavier' Junker");
 }
 
 const TCHAR * OgreMaxExport::CopyrightMessage() { 
-	return _T("Clash of Steel (c) 2006");
+	return _T("The OGRE Team (c) 2006");
 }
 
 const TCHAR * OgreMaxExport::OtherMessage1() { 
