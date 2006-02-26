076f3207fb771a52262b7a2f5f2e29baadd417b4
nfz <nfz>
2006-02-26 13:57:16 +0000

    update license

--- Samples/OceanDemo/include/OceanDemo.h
+++ Samples/OceanDemo/include/OceanDemo.h
@@ -1,33 +1,16 @@
-/************************************************************************
-	filename: 	OceanDemo.h
-	created:	01 Aug 2004
-	author:		Jeff Doyle (nfz)
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
 
-	modified:
-        05 Aug 2005 - setup up config file for material controls so they are not hard coded
-        04 Aug 2005 - updated to work with Ogre SDK 1.0.3 and CEGUI 0.3.0
-                    - moved over to Ocean Demo
-        10 Jun 2005 - updated to work with Ogre SDK 1.0.2 and CEGUI 0.2.0
-		09 Aug 2004 - shader horizontal scroll bars automatically configured
-					- added more shaders
-		07 Aug 2004	- all windows loaded through cegui.config
-		06 Aug 2004 - moved all .xsd and .xml into ..\..\media\gui directory
-		01 Aug 2004	- using CEGUI Demo7 as a base
+Copyright (c) 2000-2006 The OGRE Team
+Also see acknowledgements in Readme.html
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
-
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*************************************************************************/
+You may use this sample code for anything you like, it is not covered by the
+LGPL like the rest of the engine.
+-----------------------------------------------------------------------------
+*/
 
 #ifndef _OceanDemo_H_
 #define _OceanDemo_H_
