24d936ed161a08a6731410a2ba9b3a7f00e8a880
nfz <nfz@0896a727-e8d4-4d2b-8422-44d28760243c>
2006-02-26 13:57:16 +0000

    update license

--- trunk/Samples/OceanDemo/include/MaterialControls.h
+++ trunk/Samples/OceanDemo/include/MaterialControls.h
@@ -1,25 +1,16 @@
-/************************************************************************
-	filename: 	MaterialControls.h
-	created:	05 Aug 2005
-	author:		Jeff Doyle (nfz)
-	
-	modified:
-        05 Aug 2005 - file created
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+    (Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
+Copyright (c) 2000-2006 The OGRE Team
+Also see acknowledgements in Readme.html
 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
-
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-*************************************************************************/
+You may use this sample code for anything you like, it is not covered by the
+LGPL like the rest of the engine.
+-----------------------------------------------------------------------------
+*/
 
 #ifndef __MaterialControls_H__
 #define __MaterialControls_H__
