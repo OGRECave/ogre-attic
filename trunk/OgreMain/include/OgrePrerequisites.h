c289d0778758d7f212e7fa03b7846c35c511783b
sinbad <sinbad@0896a727-e8d4-4d2b-8422-44d28760243c>
2006-03-12 12:18:34 +0000

    Bump CVS HEAD to Eihort, Dagon is now on branch 'v1-2'

--- trunk/OgreMain/include/OgrePrerequisites.h
+++ trunk/OgreMain/include/OgrePrerequisites.h
@@ -93,9 +93,9 @@ http://www.gnu.org/copyleft/lesser.txt
 namespace Ogre {
     // Define ogre version
     #define OGRE_VERSION_MAJOR 1
-    #define OGRE_VERSION_MINOR 2
+    #define OGRE_VERSION_MINOR 3
     #define OGRE_VERSION_PATCH 0
-    #define OGRE_VERSION_NAME "Dagon"
+    #define OGRE_VERSION_NAME "Eihort"
 
     #define OGRE_VERSION    ((OGRE_VERSION_MAJOR << 16) | (OGRE_VERSION_MINOR << 8) | OGRE_VERSION_PATCH)
 
