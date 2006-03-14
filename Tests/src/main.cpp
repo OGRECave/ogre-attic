a576d3781dd9d3672b0978f7be09c81621748c39
nfz <nfz>
2006-03-14 01:37:42 +0000

    - added try/catch blocks to catch exceptions not caught by cppunit - setup ogre logger in main to log all events

--- Tests/src/main.cpp
+++ Tests/src/main.cpp
@@ -1,11 +1,37 @@
 #include <cppunit/extensions/TestFactoryRegistry.h>
 #include <cppunit/ui/text/TestRunner.h>
+#include "OgreException.h"
+#include "OgreLogManager.h"
+
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+#define WIN32_LEAN_AND_MEAN
+#include "windows.h"
+#endif
 
 int main( int argc, char **argv)
 {
-    CppUnit::TextUi::TestRunner runner;
-    CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
-    runner.addTest( registry.makeTest() );
-    bool wasSuccessful = runner.run( "", false );
-    return wasSuccessful ? 0 : 1;
+    std::auto_ptr<Ogre::LogManager> logMgr(new Ogre::LogManager());
+    logMgr->createLog("OgreTest.log", true, true);
+
+    try {
+        CppUnit::TextUi::TestRunner runner;
+        CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
+        runner.addTest( registry.makeTest() );
+        bool wasSuccessful = runner.run( "", false );
+        return wasSuccessful ? 0 : 1;
+    }
+    catch( Ogre::Exception& e ) {
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+        MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
+        std::cerr << "An exception has occured: " << e.getFullDescription();
+#endif
+    }
+    catch( ... ) {
+#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
+        MessageBox( NULL, "Unknown exception", "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
+#else
+        std::cerr << "An exception has occured: " << e.getFullDescription();
+#endif
+    }
 }
