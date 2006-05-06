478ec19bfa1ee3df6fe117ad726aad7d05086dbe
nfz <nfz>
2006-05-06 21:57:21 +0000

    - added many more test examples for Material Script compiler - create the different managers on demand

--- Tests/OgreMain/src/MaterialScriptCompilerTests.cpp
+++ Tests/OgreMain/src/MaterialScriptCompilerTests.cpp
@@ -24,6 +24,8 @@ http://www.gnu.org/copyleft/lesser.txt.
 */
 
 #include "OgreMaterialManager.h"
+#include "OgreHighLevelGpuProgramManager.h"
+#include "OgreGpuProgramManager.h"
 #include "MaterialScriptCompilerTests.h"
 #include "OgreStringConverter.h"
 
@@ -127,12 +129,28 @@ void MaterialScriptCompilerTests::testIsLexemeMatch(void)
 void MaterialScriptCompilerTests::testCompileMaterialScript()
 {
     Math* mth = new Math();
-    ResourceGroupManager* resGrpMgr = new ResourceGroupManager();
-    MaterialManager* matMgr = new MaterialManager();
-    matMgr->initialise();
+    if (!ResourceGroupManager::getSingletonPtr())
+        new ResourceGroupManager();
+
+    if (!MaterialManager::getSingletonPtr())
+    {
+        new MaterialManager();
+        MaterialManager::getSingleton().initialise();
+    }
+
+    if (!HighLevelGpuProgramManager::getSingletonPtr())
+        new HighLevelGpuProgramManager();
+
+    //if (!GpuProgramManager::getSingletonPtr())
+    //    new GpuProgramManager();
+
 
     const String simpleScript =
-        "material test2 { technique { \n"
+        "material test2 { \n"
+        " receive_shadows on lod_distances 100 203.6 700 \n"
+        " set_texture_alias foo-bar MyTexture.png \n"
+        " transparency_casts_shadows on \n"
+        " technique { scheme hdr lod_index 1 \n"
         " pass pass-1 { \n"
         "   ambient 0 1 0.5\n"
         "   ambient 0 1 0.5 1 \n"
@@ -146,18 +164,35 @@ void MaterialScriptCompilerTests::testCompileMaterialScript()
         "   depth_check on \n"
         "   depth_write off \n"
         "   depth_func less_equal \n"
+        "   depth_bias 5 \n"
         "   alpha_rejection not_equal 127 \n"
         "   cull_hardware clockwise \n"
+        "   cull_software front \n"
         "   lighting on \n"
         "   shading gouraud \n"
         "   polygon_mode wireframe \n"
+        "   fog_override false \n"
+        "   fog_override true exp 1 1 1 0.002 100 10000 \n"
+        "   fog_override true \n"
+        "   colour_write off \n"
+        "   max_lights 7 \n"
+        "   iteration once \n"
+        "   iteration once_per_light point \n"
+        "   iteration 5 \n"
+        "   iteration 5 per_light point \n"
+        "   point_size 3 \n"
+        "   point_sprites on \n"
+        "   point_size_attenuation on \n"
+        "   point_size_min 1.00 \n"
+        "   point_size_max 10 \n"
         "   texture_unit first {texture MyAlphaTexture.png alpha tex_coord_set 0 } \n"
         "   texture_unit first { \n"
-        "       cubic_texture cubemap_fr.jpg cubemap_bk.jpg cubemap_lf.jpg cubemap_rt.jpg cubemap_up.jpg cubemap_dn.jpg separateUV \n"
+        "       cubic_texture cubemap_fr.jpg \t cubemap_bk.jpg cubemap_lf.jpg cubemap_rt.jpg cubemap_up.jpg cubemap_dn.jpg separateUV \n"
         "   } \n"
         "   texture_unit \"second one\" { \n"
+        "       texture_alias foo-bar \n"
         "       anim_texture MyAlphaTexture2.png 4 6.5 \n"
-        "       anim_texture MyAlphaTexture2.png MyAlphaTexture6.png MyAlphaTexture9.png 6.5 \n"
+        "       anim_texture MyAlphaTexture2.png MyAlphaTexture6.png    MyAlphaTexture9.png 6.5 \n"
         "       tex_address_mode wrap \n"
         "       tex_address_mode wrap mirror \n"
         "       tex_address_mode wrap mirror border \n"
@@ -173,7 +208,7 @@ void MaterialScriptCompilerTests::testCompileMaterialScript()
         "       alpha_op_ex add_signed src_manual src_current 0.5 \n"
         "       env_map cubic_reflection \n"
         "       scroll 5 2 \n"
-        "       scroll_anim 3 2 \n"
+        "       scroll_anim  2 4 \n"
         "       rotate 2.3 \n"
         "       rotate_anim 0.5 \n"
         "       scale 1 1.4 \n"
@@ -181,14 +216,32 @@ void MaterialScriptCompilerTests::testCompileMaterialScript()
         "       transform 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 \n"
 
         "       } \n"
+        "   vertex_program_ref TestVertProg { \n"
+        "   } \n"
         "   texture_unit \"second one\" { \n"
         "       anim_texture MyAlphaTexture2.png MyAlphaTexture3.png 6.5} \n"
         " \n"
-        " } } }";
+        " } } } \n"
+        "material clone 1 : test2 {} \n"
+        // GPU Programs
+        "vertex_program tiny hlsl { \n"
+        "  source program.hlsl \n"
+        "  entry_point main \n"
+        "  syntax vs_1_1 \n"
+        "  default_params { \n"
+        "    param_named google float4 0 1 2 3 \n"
+        "    param_indexed 1 int 0 \n"
+        "    param_indexed_auto 0 blah 0 \n"
+        "    param_named_auto chicken mustard \n"
+        "  } \n"
+        " } \n"
+        "fragment_program better glsl { \n"
+        "   source program.glsl \n"
+        "   attach specular.glsl \n"
+        " } \n"
+        ;
     CPPUNIT_ASSERT(compile(simpleScript, "MaterialScriptTest"));
 
-    delete matMgr;
-    delete resGrpMgr;
     delete mth;
 }
 
