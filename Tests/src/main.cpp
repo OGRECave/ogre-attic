d6f1ad90b7289769ca8f15882243482e108d5a47
nfz <nfz>
2006-05-06 21:59:17 +0000

    do safer deletion of managers

--- Tests/src/main.cpp
+++ Tests/src/main.cpp
@@ -2,6 +2,10 @@
 #include <cppunit/ui/text/TestRunner.h>
 #include "OgreException.h"
 #include "OgreLogManager.h"
+#include "OgreMaterialManager.h"
+#include "OgreCompositorManager.h"
+#include "OgreHighLevelGpuProgramManager.h"
+#include "OgreGpuProgramManager.h"
 
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
 #define WIN32_LEAN_AND_MEAN
@@ -13,12 +17,12 @@ int main( int argc, char **argv)
     std::auto_ptr<Ogre::LogManager> logMgr(new Ogre::LogManager());
     logMgr->createLog("OgreTest.log", true, true);
 
+    bool wasSuccessful = false;
     try {
         CppUnit::TextUi::TestRunner runner;
         CppUnit::TestFactoryRegistry &registry = CppUnit::TestFactoryRegistry::getRegistry();
         runner.addTest( registry.makeTest() );
-        bool wasSuccessful = runner.run( "", false );
-        return wasSuccessful ? 0 : 1;
+        wasSuccessful = runner.run( "", false );
     }
     catch( Ogre::Exception& e ) {
 #if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
@@ -34,4 +38,13 @@ int main( int argc, char **argv)
         std::cerr << "An exception has occured: " << e.getFullDescription();
 #endif
     }
+
+    // shutdown and release managers that might have been created
+    delete Ogre::HighLevelGpuProgramManager::getSingletonPtr();
+    delete Ogre::GpuProgramManager::getSingletonPtr();
+    delete Ogre::CompositorManager::getSingletonPtr();
+    delete Ogre::MaterialManager::getSingletonPtr();
+    delete Ogre::ResourceGroupManager::getSingletonPtr();
+
+    return wasSuccessful ? 0 : 1;
 }
