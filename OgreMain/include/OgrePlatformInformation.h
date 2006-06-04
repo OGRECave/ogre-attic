1ee5ba033b7936969401e85e6cd7578b405e3a74
genva <genva>
2006-06-04 14:53:51 +0000

    Disable build SSE support for gcc at the moment, need more testing before enabling.

--- OgreMain/include/OgrePlatformInformation.h
+++ OgreMain/include/OgrePlatformInformation.h
@@ -81,7 +81,7 @@ namespace Ogre {
 #if OGRE_DOUBLE_PRECISION == 0 && OGRE_CPU == OGRE_CPU_X86 && OGRE_COMPILER == OGRE_COMPILER_MSVC
 #   define __OGRE_HAVE_SSE  1
 #elif OGRE_DOUBLE_PRECISION == 0 && OGRE_CPU == OGRE_CPU_X86 && OGRE_COMPILER == OGRE_COMPILER_GNUC
-#   define __OGRE_HAVE_SSE  1
+#   define __OGRE_HAVE_SSE  0   // Disable for now, will enable when it get ready.
 #endif
 
 
