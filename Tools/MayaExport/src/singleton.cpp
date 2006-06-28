217d002a4b30afb632a0492e213e2543c5a72725
fgiordana <fgiordana>
2006-06-28 23:21:52 +0000

    singleton.cpp no longer needed

--- Tools/MayaExport/src/singleton.cpp
+++
@@ -1,7 +0,0 @@
-// Copied frome Ogre::Singleton, created by Steve Streeting for Ogre
-#include "singleton.h"
-
-namespace OgreMayaExporter
-{
-	    //template <typename T> T* Singleton <T>::ms_Singleton = 0;
-};	//end namespace
