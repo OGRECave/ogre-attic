b90f8b98b9366e1bf6110141a112c2d3cb93f0c1
nfz <nfz@0896a727-e8d4-4d2b-8422-44d28760243c>
2006-08-26 13:07:15 +0000

    - made use of auto generated token IDs for lexeme/Action combos to make code maintenance easier. - fixed memory issue with action mapping - all tokens were being assigned an action even if the action was NULL.

--- trunk/OgreMain/include/OgreCompositorScriptCompiler.h
+++ trunk/OgreMain/include/OgreCompositorScriptCompiler.h
@@ -66,31 +66,20 @@ namespace Ogre {
 		// Token ID enumeration
 		enum TokenID {
 			// Terminal Tokens section
-			ID_UNKOWN = 0, ID_OPENBRACE, ID_CLOSEBRACE,
-			// Top level
-			ID_COMPOSITOR,
+			ID_UNKOWN = 0, 
 			// Techniques
-			ID_TECHNIQUE, ID_TEXTURE, ID_TARGET_WIDTH, ID_TARGET_HEIGHT,
+			ID_TARGET_WIDTH, ID_TARGET_HEIGHT,
 			ID_PF_A8R8G8B8, ID_PF_R8G8B8A8, ID_PF_R8G8B8,
 			ID_PF_FLOAT16_R, ID_PF_FLOAT16_RGB, ID_PF_FLOAT16_RGBA,
 			ID_PF_FLOAT32_R, ID_PF_FLOAT32_RGB, ID_PF_FLOAT32_RGBA,
 			// Targets
-			ID_TARGET, ID_INPUT, ID_TARGET_OUTPUT, ID_ONLY_INITIAL,
-			ID_VISIBILITY_MASK, ID_LOD_BIAS, ID_MATERIAL_SCHEME,
 			ID_PREVIOUS, ID_NONE,
 			// Passes
-			ID_PASS,
-			ID_MATERIAL,
 			ID_RENDER_QUAD, ID_CLEAR, ID_STENCIL, ID_RENDER_SCENE,
-			ID_FIRST_RQ, ID_LAST_RQ,
-			ID_IDENTIFIER,
-			// clear
-			ID_CLR_BUFF, ID_CLR_COLOUR, ID_CLR_DEPTH,
-			ID_CLR_COLOUR_VAL, ID_CLR_DEPTH_VAL, ID_CLR_STENCIL_VAL,
-			// stencil
-			ID_ST_CHECK, ID_ST_FUNC, ID_ST_REF_VAL, ID_ST_MASK, ID_ST_FAILOP,
-			ID_ST_DEPTH_FAILOP, ID_ST_PASSOP, ID_ST_TWOSIDED,
-
+			// Clear section
+			ID_CLR_COLOUR, ID_CLR_DEPTH,
+			// Stencil section
+			
 			// compare functions
             ID_ST_ALWAYS_FAIL, ID_ST_ALWAYS_PASS, ID_ST_LESS,
             ID_ST_LESS_EQUAL, ID_ST_EQUAL, ID_ST_NOT_EQUAL,
@@ -102,7 +91,9 @@ namespace Ogre {
             ID_ST_INVERT,
 
 			// general
-			ID_ON, ID_OFF, ID_TRUE, ID_FALSE
+			ID_ON, ID_OFF, ID_TRUE, ID_FALSE,
+            // where auto generated tokens start so donot remove
+            ID_AUTOTOKENSTART
 		};
 
 		/** Enum to identify compositor sections. */
@@ -145,10 +136,14 @@ namespace Ogre {
 		section of the source that has been parsed.
 		**/
 		virtual void executeTokenAction(const size_t tokenID);
+        /** Get the start position of auto generated token IDs.
+        */
+        virtual size_t getAutoTokenIDStart() const {return ID_AUTOTOKENSTART;}
 		/** Associate all the lexemes used in a material script with their corresponding tokens and actions.
 		**/
         virtual void setupTokenDefinitions(void);
 		void addLexemeTokenAction(const String& lexeme, const size_t token, const CSC_Action action = 0);
+        void addLexemeAction(const String& lexeme, const CSC_Action action) { addLexemeTokenAction(lexeme, 0, action); }
 
 		void logParseError(const String& error);
 
