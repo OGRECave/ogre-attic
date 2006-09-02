cf987e37c701beb452ada34a2bf0652d0c44b4fc
sinbad <sinbad>
2006-09-02 11:43:19 +0000

    Viewport clear buffers should default to FBT_COLOUR|FBT_DEPTH, the same as the value defaulted in Viewport::setClearEveryFrame, not 0xFF. Not only was this inconsistent, it was causing repeated recompilation of the shared 'original scene' compositor instance because of differences

--- OgreMain/src/OgreViewport.cpp
+++ OgreMain/src/OgreViewport.cpp
@@ -46,7 +46,7 @@ namespace Ogre {
         , mZOrder(ZOrder)
         , mBackColour(ColourValue::Black)
         , mClearEveryFrame(true)
-		, mClearBuffers(0xFF)
+		, mClearBuffers(FBT_COLOUR | FBT_DEPTH)
         , mUpdated(false)
         , mShowOverlays(true)
         , mShowSkies(true)
