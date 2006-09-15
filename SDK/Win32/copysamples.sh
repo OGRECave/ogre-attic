98c9e241c74d138b85b90d1fa28acd0fade51065
nfz <nfz>
2006-09-15 05:47:22 +0000

    process all CB projects.  hmmm...maybe this shell script should take a parameter for SDK build environment so that only relevant files are processed.  Maybe that will be the next commit :).

--- SDK/Win32/copysamples.sh
+++ SDK/Win32/copysamples.sh
@@ -10,21 +10,22 @@ mkdir samples/src
 mkdir samples/include
 
 # Do the project files
-/bin/find ../../samples -iname *_stlp.cbp -exec cp \{\} samples/scripts \;
+/bin/find ../../samples -iname *.cbp -exec cp \{\} samples/scripts \;
 /bin/find ../../samples -iname *.vcproj -exec cp \{\} samples/scripts \;
 /bin/find ../../samples -iname *_vc8.vcproj.user -exec cp \{\} samples/scripts \;
-cp ../../ReferenceApplication/BspCollision/scripts/*_stlp.cbp samples/scripts
+cp ../../ReferenceApplication/BspCollision/scripts/*.cbp samples/scripts
 cp ../../ReferenceApplication/BspCollision/scripts/*.vcproj samples/scripts
 cp ../../ReferenceApplication/BspCollision/scripts/*_vc8.vcproj.user samples/scripts
-cp ../../ReferenceApplication/ReferenceAppLayer/scripts/*_stlp.cbp samples/refapp/scripts
+cp ../../ReferenceApplication/ReferenceAppLayer/scripts/*.cbp samples/refapp/scripts
 cp ../../ReferenceApplication/ReferenceAppLayer/scripts/*.vcproj samples/refapp/scripts
 rm samples/scripts/OgreGUIRenderer_stlp.cbp
+rm samples/scripts/OgreGUIRenderer.cbp
 rm samples/scripts/OgreGUIRenderer.vcproj
 rm samples/scripts/OgreGUIRenderer_vc8.vcproj
-/bin/find samples/scripts/ -iname *_stlp.cbp -exec sed -i -f altersamples.sed \{\} \;
+/bin/find samples/scripts/ -iname *.cbp -exec sed -i -f altersamples.sed \{\} \;
 /bin/find samples/scripts/ -iname *.vcproj -exec sed -i -f altersamples.sed \{\} \;
 /bin/find samples/scripts/ -iname *_vc8.vcproj.user -exec sed -i -f altersamples.sed \{\} \;
-/bin/find samples/refapp/scripts/ -iname *_stlp.cbp -exec sed -i -f alterrefapp.sed \{\} \;
+/bin/find samples/refapp/scripts/ -iname *.cbp -exec sed -i -f alterrefapp.sed \{\} \;
 /bin/find samples/refapp/scripts/ -iname *.vcproj -exec sed -i -f alterrefapp.sed \{\} \;
 
 # Combine the include / src folders; easier to do here than in setup
