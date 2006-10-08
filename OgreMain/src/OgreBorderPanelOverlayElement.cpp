e10d1f679bbb06beeeac4d809368e71db3640dfa
sinbad <sinbad>
2006-10-08 22:39:14 +0000

    Fix incorrect static_cast<unsigned> instances in BorderPanelOverlayElement

--- OgreMain/src/OgreBorderPanelOverlayElement.cpp
+++ OgreMain/src/OgreBorderPanelOverlayElement.cpp
@@ -237,7 +237,7 @@ namespace Ogre {
         else
         {
             mLeftBorderSize = mRightBorderSize = 
-                mTopBorderSize = mBottomBorderSize = static_cast<unsigned>(size);
+                mTopBorderSize = mBottomBorderSize = size;
         }
         mGeomPositionsOutOfDate = true;
     }
@@ -251,8 +251,8 @@ namespace Ogre {
         }
         else
         {
-            mLeftBorderSize = mRightBorderSize = static_cast<unsigned>(sides);
-            mTopBorderSize = mBottomBorderSize = static_cast<unsigned>(topAndBottom);
+            mLeftBorderSize = mRightBorderSize = sides;
+            mTopBorderSize = mBottomBorderSize = topAndBottom;
         }
         mGeomPositionsOutOfDate = true;
 
@@ -270,10 +270,10 @@ namespace Ogre {
         }
         else
         {
-            mLeftBorderSize = static_cast<unsigned>(left);
-            mRightBorderSize = static_cast<unsigned>(right);
-            mTopBorderSize = static_cast<unsigned>(top);
-            mBottomBorderSize = static_cast<unsigned>(bottom);
+            mLeftBorderSize = left;
+            mRightBorderSize = right;
+            mTopBorderSize = top;
+            mBottomBorderSize = bottom;
         }
         mGeomPositionsOutOfDate = true;
     }
