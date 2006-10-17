dec461579f27a2eabf9fcb6faec64140bac0d9d7
genva <genva>
2006-10-17 03:35:30 +0000

    Fixed quaternion shortest path slerp bug, also use lerp rather than just return first quaternion in case two quaternions are very close.

--- OgreMain/src/OgreQuaternion.cpp
+++ OgreMain/src/OgreQuaternion.cpp
@@ -428,27 +428,41 @@ namespace Ogre {
         const Quaternion& rkQ, bool shortestPath)
     {
         Real fCos = rkP.Dot(rkQ);
-        Radian fAngle ( Math::ACos(fCos) );
+        Quaternion rkT;
 
-        if ( Math::Abs(fAngle.valueRadians()) < ms_fEpsilon )
-            return rkP;
-
-        Real fSin = Math::Sin(fAngle);
-        Real fInvSin = 1.0/fSin;
-        Real fCoeff0 = Math::Sin((1.0-fT)*fAngle)*fInvSin;
-        Real fCoeff1 = Math::Sin(fT*fAngle)*fInvSin;
         // Do we need to invert rotation?
         if (fCos < 0.0f && shortestPath)
         {
-            fCoeff0 = -fCoeff0;
-            // taking the complement requires renormalisation
-            Quaternion t(fCoeff0*rkP + fCoeff1*rkQ);
-            t.normalise();
-            return t;
+            fCos = -fCos;
+            rkT = -rkQ;
         }
         else
         {
-            return fCoeff0*rkP + fCoeff1*rkQ;
+            rkT = rkQ;
+        }
+
+        if (Math::Abs(fCos) < 1 - ms_fEpsilon)
+        {
+            // Standard case (slerp)
+            Real fSin = Math::Sqrt(1 - Math::Sqr(fCos));
+            Radian fAngle = Math::ATan2(fSin, fCos);
+            Real fInvSin = 1.0f / fSin;
+            Real fCoeff0 = Math::Sin((1.0f - fT) * fAngle) * fInvSin;
+            Real fCoeff1 = Math::Sin(fT * fAngle) * fInvSin;
+            return fCoeff0 * rkP + fCoeff1 * rkT;
+        }
+        else
+        {
+            // There are two situations:
+            // 1. "rkP" and "rkQ" are very close (fCos ~= +1), so we can do a linear
+            //    interpolation safely.
+            // 2. "rkP" and "rkQ" are almost inverse of each other (fCos ~= -1), there
+            //    are an infinite number of possibilities interpolation. but we haven't
+            //    have method to fix this case, so just use linear interpolation here.
+            Quaternion t = (1.0f - fT) * rkP + fT * rkT;
+            // taking the complement requires renormalisation
+            t.normalise();
+            return t;
         }
     }
     //-----------------------------------------------------------------------
