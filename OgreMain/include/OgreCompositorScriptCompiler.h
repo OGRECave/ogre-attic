4c0f9a48dde9e309fe7bb80472e98c57d5f2aaa7
nfz <nfz>
2006-11-25 19:20:25 +0000

    fixed error in license.  Error was reported by admins on www.koders.com.

--- OgreMain/include/OgreCompositorScriptCompiler.h
+++ OgreMain/include/OgreCompositorScriptCompiler.h
@@ -2,24 +2,28 @@
 -----------------------------------------------------------------------------
 This source file is part of OGRE
 (Object-oriented Graphics Rendering Engine)
-For the latest info, see http://www.stevestreeting.com/ogre/
+For the latest info, see http://ogre.sourceforge.net/
 
 Copyright (c) 2000-2006 Torus Knot Software Ltd
 Also see acknowledgements in Readme.html
 
 This program is free software; you can redistribute it and/or modify it under
-the terms of the GNU General Public License as published by the Free Software
+the terms of the GNU Lesser General Public License as published by the Free Software
 Foundation; either version 2 of the License, or (at your option) any later
 version.
 
 This program is distributed in the hope that it will be useful, but WITHOUT
 ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with
+You should have received a copy of the GNU Lesser General Public License along with
 this program; if not, write to the Free Software Foundation, Inc., 59 Temple
 Place - Suite 330, Boston, MA 02111-1307, USA, or go to
-http://www.gnu.org/copyleft/gpl.html.
+http://www.gnu.org/copyleft/lesser.txt.
+
+You may alternatively use this source under the terms of a specific version of
+the OGRE Unrestricted License provided you have obtained such a license from
+Torus Knot Software Ltd.
 -----------------------------------------------------------------------------
 */
 
@@ -66,7 +70,7 @@ namespace Ogre {
 		// Token ID enumeration
 		enum TokenID {
 			// Terminal Tokens section
-			ID_UNKOWN = 0, 
+			ID_UNKOWN = 0,
 			// Techniques
 			ID_TARGET_WIDTH, ID_TARGET_HEIGHT,
 			ID_PF_A8R8G8B8, ID_PF_R8G8B8A8, ID_PF_R8G8B8,
@@ -79,7 +83,7 @@ namespace Ogre {
 			// Clear section
 			ID_CLR_COLOUR, ID_CLR_DEPTH,
 			// Stencil section
-			
+
 			// compare functions
             ID_ST_ALWAYS_FAIL, ID_ST_ALWAYS_PASS, ID_ST_LESS,
             ID_ST_LESS_EQUAL, ID_ST_EQUAL, ID_ST_NOT_EQUAL,
