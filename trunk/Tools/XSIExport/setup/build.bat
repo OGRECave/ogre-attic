6304751bf0f3156778a67083bd76f7465b4917bb
sinbad <sinbad@0896a727-e8d4-4d2b-8422-44d28760243c>
2007-02-17 16:05:13 +0000

    Add XSI 6 install build target and update for Eihort

--- trunk/Tools/XSIExport/setup/build.bat
+++ trunk/Tools/XSIExport/setup/build.bat
@@ -1,5 +1,12 @@
 @echo off
 if "%1" == "4" goto xsi4
+if "%1" == "5" goto xsi5
+
+:xsi6
+candle xsi6install.wxs
+light -out OgreXSIExporter6.msi xsi6install.wixobj %WIX_ROOT%\ui\wixui_featuretree.wixlib
+goto end
+
 
 :xsi5
 candle xsi5install.wxs
