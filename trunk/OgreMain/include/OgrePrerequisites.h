8c58399daf60eb6a3b7cb799f573c746cf3b0930
sinbad <sinbad@0896a727-e8d4-4d2b-8422-44d28760243c>
2007-03-15 14:10:00 +0000

    Make #ifdef more specific on OgrePrerequisites.h to avoid name clashes

--- trunk/OgreMain/include/OgrePrerequisites.h
+++ trunk/OgreMain/include/OgrePrerequisites.h
@@ -22,8 +22,8 @@ along with this library; if not, write to the Free Software Foundation,
 Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307 USA or go to
 http://www.gnu.org/copyleft/lesser.txt
 -------------------------------------------------------------------------*/
-#ifndef __Prerequisites_H__
-#define __Prerequisites_H__
+#ifndef __OgrePrerequisites_H__
+#define __OgrePrerequisites_H__
 
 // undefine this to not require new angular units where applicable
 #define OGRE_FORCE_ANGLE_TYPES
