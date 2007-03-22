c7abe39392b034e234536d72cdd0ce21d9f673ee
nfz <nfz@0896a727-e8d4-4d2b-8422-44d28760243c>
2007-03-22 01:33:50 +0000

    Removed termHandler - no longer implemented.

--- trunk/OgreMain/include/OgreRoot.h
+++ trunk/OgreMain/include/OgreRoot.h
@@ -171,8 +171,6 @@ namespace Ogre
         Real calculateEventTime(unsigned long now, FrameEventTimeType type);
     public:
 
-        static void termHandler();
-
         /** Constructor
         @param pluginFileName The file that contains plugins information.
             Defaults to "plugins.cfg", may be left blank to ignore.
