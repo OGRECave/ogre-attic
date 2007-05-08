fef45e52caaef92490f507f7a04c906f25efe0f0
mark_folkenberg <mark_folkenberg>
2007-05-08 17:17:30 +0000

    - Corrected some project settings so it will compile out of the box - Added short description of build requirments in !DEPENDENCIES.txt

--- Tools/3dsmaxExport/LEXIExporter/LexiExport/Sources/LexiIntermediateMesh.h
+++ Tools/3dsmaxExport/LEXIExporter/LexiExport/Sources/LexiIntermediateMesh.h
@@ -109,6 +109,9 @@ class CIntermediateMesh : public Ogre::MovableObject {
 		//
 		//  Triangle
 
+		// MovableObject implementation
+		void visitRenderables(Ogre::Renderable::Visitor* visitor,	bool debugRenderables = false){};
+
 		// Get number of triangles
 		unsigned int GetNumTriangles() const;
 
