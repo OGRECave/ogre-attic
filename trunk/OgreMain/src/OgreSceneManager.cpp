88f067a195f567b3c1055e7611dcc4507eb15ba3
sinbad <sinbad@0896a727-e8d4-4d2b-8422-44d28760243c>
2007-06-13 17:42:46 +0000

    Eihort merged into HEAD

--- trunk/OgreMain/src/OgreSceneManager.cpp
+++ trunk/OgreMain/src/OgreSceneManager.cpp
@@ -2700,12 +2700,15 @@ void SceneManager::renderSingleObject(const Renderable* rend, const Pass* pass,
 
 			// deliberately unsigned in case start light exceeds number of lights
 			// in which case this pass would be skipped
-			int lightsLeft;
-			lightsLeft = static_cast<int>(rendLightList.size()) - pass->getStartLight();
-			// Don't allow total light count for all iterations to exceed max per pass
-			if (lightsLeft > static_cast<int>(pass->getMaxSimultaneousLights()))
+			int lightsLeft = 1;
+			if (iteratePerLight)
 			{
-				lightsLeft = static_cast<int>(pass->getMaxSimultaneousLights());
+				lightsLeft = static_cast<int>(rendLightList.size()) - pass->getStartLight();
+				// Don't allow total light count for all iterations to exceed max per pass
+				if (lightsLeft > static_cast<int>(pass->getMaxSimultaneousLights()))
+				{
+					lightsLeft = static_cast<int>(pass->getMaxSimultaneousLights());
+				}
 			}
 
 
