41fad443c6fbf807360505dcb0540e144b22ebb2
hudson_b <hudson_b@0896a727-e8d4-4d2b-8422-44d28760243c>
2007-06-18 03:27:59 +0000

    Added additional comments (copywright etc)
    Initial event firing

--- trunk/Tools/MaterialEditor/MaterialEditor/include/Project.h
+++ trunk/Tools/MaterialEditor/MaterialEditor/include/Project.h
@@ -1,5 +1,37 @@
-#ifndef PROJECT_H
-#define PROJECT_H
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+(Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+
+Copyright (c) 2000-2006 Torus Knot Software Ltd
+Also see acknowledgements in Readme.html
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+http://www.gnu.org/copyleft/lesser.txt.
+
+You may alternatively use this source under the terms of a specific version of
+the OGRE Unrestricted License provided you have obtained such a license from
+Torus Knot Software Ltd.
+-----------------------------------------------------------------------------
+*/
+#ifndef _PROJECT_H_
+#define _PROJECT_H_
+
+#include <list>
+
+#include <boost/signal.hpp>
 
 #include "OgreString.h"
 
@@ -9,10 +41,27 @@ namespace Ogre
 	class SceneManager;
 }
 
+class MaterialController;
+class Project;
+
 using Ogre::Material;
 using Ogre::String;
 
-typedef std::list<Material*> MaterialList;
+typedef std::list<MaterialController*> MaterialControllerList;
+
+
+typedef boost::signal<void (Project*, const String&)> NameChangedDelegate;
+typedef boost::signal<void (Project*, MaterialController*)> MaterialAddedDelegate;
+typedef boost::signal<void (Project*, MaterialController*)> MaterialRemovedDelegate;
+typedef boost::signal<void (Project*, MaterialController*)> ActiveMaterialChangedDelegate;
+
+enum ProjectEvent
+{
+	NamedChanged,
+	MaterialAdded,
+	MaterialRemoved,
+	ActiveMaterialChanged
+};
 
 class Project
 {
@@ -23,9 +72,20 @@ public:
 	const String& getName() const;
 	void setName(const String& name);
 
-	Material* getActiveMaterial() const;
+	void addMaterial(Material* material);
+
+	void removeMaterial(MaterialController* controller);
+	void removeMaterial(Material* material);
+	void removeMaterial(const String& name);
+	
+	MaterialController* getActiveMaterial() const;
+	void setActiveMaterial(MaterialController* controller);
 	void setActiveMaterial(Material* material);
 	void setActiveMaterial(const String& name);
+	
+	MaterialController* getMaterialController(const String& name);
+	
+	const MaterialControllerList* getMaterials() const;
 
 	void open();
 	void close();
@@ -34,11 +94,41 @@ public:
 	bool isClosed();
 
 	void generateScene(Ogre::SceneManager* sceneManager);
+	
+	// use boost::bind for the functor so tracking is enabled
+	template<class T>
+	void subscribe(ProjectEvent event, T& functor)
+	{
+		switch(event)
+		{
+		case ProjectEvent::NameChanged:
+				mNameChanged.connect(functor);
+			break;
+			
+		case ProjectEvent::MaterialAdded:
+				mMaterialAdded.connect(functor);
+		break;
+			
+		case ProjectEvent::MaterialRemoved:
+				mMaterialRemoved.connect(functor);
+		break;
+			
+		case ProjectEvent::ActiveMaterialChanged:
+				mActiveMaterialChanged.connect(functor);
+		break;
+		}
+	}
 
 protected:
 	String mName;
 	bool mOpen;
-	Material* mActiveMaterial;
+	MaterialController* mActiveMaterial;
+	MaterialControllerList mMaterialControllers;
+	
+	NameChangedDelegate mNameChanged;
+	MaterialAddedDelegate mMaterialAdded;
+	MaterialRemovedDelegate mMaterialRemoved;
+	ActiveMaterialChangedDelegate mActiveMaterialChanged;
 };
 
-#endif // PROJECT_H
+#endif // _PROJECT_H_
