a625989dc32c382ce321d2b2a5097beb4dc12726
hudson_b <hudson_b@0896a727-e8d4-4d2b-8422-44d28760243c>
2007-06-18 03:18:17 +0000

    Added additional comments (copywright etc)
    Initial event firing

--- trunk/Tools/MaterialEditor/MaterialEditor/src/Project.cpp
+++ trunk/Tools/MaterialEditor/MaterialEditor/src/Project.cpp
@@ -1,19 +1,54 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+(Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+
+Copyright (c) 2000-2006 Torus Knot Software Ltd
+Also see acknowledgements in Readme.html
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+http://www.gnu.org/copyleft/lesser.txt.
+
+You may alternatively use this source under the terms of a specific version of
+the OGRE Unrestricted License provided you have obtained such a license from
+Torus Knot Software Ltd.
+-----------------------------------------------------------------------------
+*/
 #include "Project.h"
 
 #include "OgreSceneManager.h"
 #include "OgreMaterial.h"
 #include "OgreMaterialManager.h"
 
+#include "MaterialController.h"
+
 using Ogre::MaterialManager;
 
 Project::Project() : mActiveMaterial(NULL)
 {
-
 }
 
 Project::~Project()
 {
-
+	MaterialControllerList::iterator it;
+	for(it = mMaterialControllers.begin(); it != mMaterialControllers.end(); ++it)
+	{
+		delete *it;
+	}
+	
+	mMaterialControllers.clear();
 }
 
 const String& Project::getName() const
@@ -24,39 +59,90 @@ const String& Project::getName() const
 void Project::setName(const String& name)
 {
 	mName = name;
+	
+	mNameChanged(this, name);
 }
 
-Material* Project::getActiveMaterial() const
+void Project::addMaterial(Material* material)
+{
+	MaterialController* controller = new MaterialController(material);
+	mMaterialControllers.push_back(controller);
+	
+	mMaterialAdded(this, controller);
+}
+
+void Project::removeMaterial(MaterialController* controller)
+{
+	MaterialControllerList::iterator it;
+	for(it = mMaterialControllers.begin(); it != mMaterialControllers.end(); ++it)
+	{
+		if(*it == controller) 
+		{
+			mMaterialControllers.erase(it);
+			break;
+		}
+	}
+	
+	mMaterialRemoved(this, controller);
+}
+
+void Project::removeMaterial(Material* material)
+{
+	removeMaterial(getMaterialController(material->getName()));
+}
+
+void Project::removeMaterial(const String& name)
+{
+	removeMaterial(getMaterialController(name));
+}
+
+MaterialController* Project::getActiveMaterial() const
 {
 	return mActiveMaterial;
 }
 
+void Project::setActiveMaterial(MaterialController* controller)
+{
+	assert(controller);
+	
+	if(controller == mActiveMaterial) return;
+
+	mActiveMaterial = controller;
+	
+	mActiveMaterialChanged(this, controller);
+}
+
 void Project::setActiveMaterial(Material* material)
 {
-	if(material == mActiveMaterial) return;
-
-	mActiveMaterial = material;
+	setActiveMaterial(getMaterialController(material->getName()));
 }
 
 void Project::setActiveMaterial(const String& name)
 {
-	//Material* m = (Material*)MaterialManager::getSingletonPtr()->getByName(name);
-	//if(m == NULL) return; // ERROR
+	setActiveMaterial(getMaterialController(name));
+}
 
-	//mActiveMaterial = m;
+MaterialController* Project::getMaterialController(const String& name)
+{
+	MaterialController* mc;
+	MaterialControllerList::iterator it;
+	for(it = mMaterialControllers.begin(); it != mMaterialControllers.end(); ++it)
+	{
+		mc = (*it);
+		if(mc->getMaterial()->getName() == name) return mc;
+	}
+
+	return NULL;
 }
 
 void Project::open()
 {
-
 }
 
 void Project::close()
 {
-
 }
 
 void Project::generateScene(Ogre::SceneManager* sceneManager)
 {
-
 }
