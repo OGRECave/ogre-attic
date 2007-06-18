d7204d60508bef33b61fbf5734c1e27fb6e8c8ea
hudson_b <hudson_b>
2007-06-18 03:18:17 +0000

    Added additional comments (copywright etc)
    Initial event firing

--- Tools/MaterialEditor/MaterialEditor/src/Workspace.cpp
+++ Tools/MaterialEditor/MaterialEditor/src/Workspace.cpp
@@ -1,3 +1,31 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+(Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+
+Copyright (c) 2000-2006 Torus Knot Software Ltd
+Also see acknowledgements in Readme.html
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+http://www.gnu.org/copyleft/lesser.txt.
+
+You may alternatively use this source under the terms of a specific version of
+the OGRE Unrestricted License provided you have obtained such a license from
+Torus Knot Software Ltd.
+-----------------------------------------------------------------------------
+*/
 #include "Workspace.h"
 
 #include "Project.h"
@@ -16,7 +44,6 @@ Workspace* Workspace::getSingletonPtr(void)
 
 Workspace::Workspace()
 {
-
 }
 
 Workspace::~Workspace()
@@ -31,11 +58,20 @@ Workspace::~Workspace()
 void Workspace::addProject(Project* project)
 {
 	mProjects.push_back(project);
+	
+	mProjectAdded(this, project);
 }
 
 void Workspace::removeProject(Project* project)
 {
 	mProjects.remove(project);
+	mProjectRemoved(this, project);
+	delete project;
+}
+
+void Workspace::removeProject(const String& name)
+{
+	removeProject(getProject(name));
 }
 
 Project* Workspace::getProject(const String& name)
@@ -44,7 +80,7 @@ Project* Workspace::getProject(const String& name)
 	ProjectList::iterator it;
 	for(it = mProjects.begin(); it != mProjects.end(); ++it)
 	{
-		p = *it;
+		p = (*it);
 		if(p->getName() == name) return p;
 	}
 
