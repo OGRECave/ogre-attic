f6fec0661489bb0c85d1dd2f53d591a3bb92b7ef
hudson_b <hudson_b>
2007-06-18 03:27:59 +0000

    Added additional comments (copywright etc)
    Initial event firing

--- Tools/MaterialEditor/MaterialEditor/include/Workspace.h
+++ Tools/MaterialEditor/MaterialEditor/include/Workspace.h
@@ -1,13 +1,55 @@
+/*
+-----------------------------------------------------------------------------
+This source file is part of OGRE
+(Object-oriented Graphics Rendering Engine)
+For the latest info, see http://www.ogre3d.org/
+
+Copyright (c) 2000-2006 Torus Knot Software Ltd
+Also see acknowledgements in Readme.html
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU Lesser General Public License as published by the Free Software
+Foundation; either version 2 of the License, or (at your option) any later
+version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License along with
+this program; if not, write to the Free Software Foundation, Inc., 59 Temple
+Place - Suite 330, Boston, MA 02111-1307, USA, or go to
+http://www.gnu.org/copyleft/lesser.txt.
+
+You may alternatively use this source under the terms of a specific version of
+the OGRE Unrestricted License provided you have obtained such a license from
+Torus Knot Software Ltd.
+-----------------------------------------------------------------------------
+*/
 #ifndef WORKSPACE_H
 #define WORKSPACE_H
 
 #include <list>
 
+#include <boost/signal.hpp>
+
 #include "OgreSingleton.h"
 
 class Project;
+class Workspace;
 
 typedef std::list<Project*> ProjectList;
+
+typedef boost::signal<void (Workspace*, Project*)> ProjectAddedDelegate;
+typedef boost::signal<void (Workspace*, Project*)> ProjectRemovedDelegate;
+
+enum WorkspaceEvent
+{
+	ProjectAdded,
+	ProjectRemoved
+};
+
+using Ogre::String;
 
 class Workspace : public Ogre::Singleton<Workspace>
 {
@@ -17,9 +59,25 @@ public:
 
 	void addProject(Project* project);
 	void removeProject(Project* project);
+	void removeProject(const String& name);
 
 	Project* getProject(const Ogre::String& name);
 	const ProjectList* getProjects() const;
+	
+	template<class T>
+	void subscribe(WorkspaceEvent event, T& functor)
+	{
+		switch(event)
+		{
+			case WorkspaceEvent::ProjectAdded:
+				mProjectAdded.connect(functor);
+			break;
+			
+			case WorkspaceEvent::ProjectRemoved:
+				mProjectRemoved.connect(functor);
+			break;
+		}
+	}
 
 	/** Override standard Singleton retrieval.
 	@remarks
@@ -56,6 +114,9 @@ public:
 
 protected:
 	ProjectList mProjects;
+	
+	ProjectAddedDelegate mProjectAdded;
+	ProjectRemovedDelegate mProjectRemoved;
 };
 
 #endif // WORKSPACE_H
