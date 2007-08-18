b652b209d78b94eec7a9f4b6c68456d48c11ff70
sinbad <sinbad>
2007-08-18 18:06:41 +0000

    Changing OSX SDK to a disk image (dmg) rather than a package. We need to keep a local copy of the frameworks anyway to build against the right version, and to avoid having to add 'Ogre/' in front of every header in order to reference the Ogre headers from the framework. So this means it's best to put everything in a single location anyway and prompt the user to copy frameworks where they need to. Also packagemanager can't handle installing to the user home very easily which sucks - dragging and dropping from a dmg is much better. Plus, dmgs are rather pretty.
    Still need to automate the final dmg packaging process and tweak a few more things, write docs etc.

--- SDK/OSX/editsamples.sed
+++ SDK/OSX/editsamples.sed
@@ -1,3 +1,8 @@
 s/\.\.\/\.\.\/Samples\/.*\/include/include/
 s/\.\.\/\.\.\/Samples\/.*\/src/src/
+s/\.\.\/\.\.\/Dependencies\/include\/CEGUI/..\/Frameworks\/CEGUI.framework\/Headers/
 s/\.\.\/\.\.\/Dependencies/Dependencies/
+s/\.\.\/\.\.\/OgreMain\/include/..\/Frameworks\/Ogre.framework\/Headers/
+s/path = \/Library\/Frameworks\/CEGUI\.framework\; sourceTree = \"<absolute>\"/path = ..\/Frameworks\/CEGUI\.framework\; sourceTree = SOURCE_ROOT/
+s/path = \/Library\/Frameworks\/OgreCEGUIRenderer\.framework\; sourceTree = \"<absolute>\"/path = ..\/Frameworks\/OgreCEGUIRenderer\.framework\; sourceTree = SOURCE_ROOT/
+s/path = Ogre\.framework\; sourceTree = BUILT_PRODUCTS_DIR/path = ..\/Frameworks\/Ogre.framework\; sourceTree = SOURCE_ROOT/
--- SDK/OSX/make_osx.sh
+++ SDK/OSX/make_osx.sh
@@ -5,108 +5,75 @@ xcodebuild -project ../../Mac/Ogre/Ogre.xcodeproj -alltargets -configuration Rel
 # Just release mode, debug is too big
 #xcodebuild -project ../../Mac/Ogre/Ogre.xcodeproj -alltargets -configuration Debug
 
+rm -rf sdk_contents 
+mkdir sdk_contents
+
 # frameworks
 echo Copying frameworks...
-rm -rf sdk_frameworks 
-mkdir sdk_frameworks
+mkdir sdk_contents/Frameworks
 
 # Stuff we've built
-cp -R ../../Mac/build/Release/*.framework sdk_frameworks/
+cp -R ../../Mac/build/Release/*.framework sdk_contents/Frameworks/
 
 # dependencies
-cp -R /Library/Frameworks/Cg.framework sdk_frameworks/
-cp -R /Library/Frameworks/CEGUI.framework sdk_frameworks/
+cp -R /Library/Frameworks/Cg.framework sdk_contents/Frameworks/
+cp -R /Library/Frameworks/CEGUI.framework sdk_contents/Frameworks/
 # OgreCEGUIrenderer is currently in the precompiled deps, maybe move
-cp -R /Library/Frameworks/OgreCEGUIRenderer.framework sdk_frameworks/
+cp -R /Library/Frameworks/OgreCEGUIRenderer.framework sdk_contents/Frameworks/
 
 echo Frameworks copied.
-echo Building frameworks package...
-
-/Developer/Tools/packagemaker -build -proj sdk_frameworks.pmproj -p sdk_frameworks.pkg
-
-echo Frameworks package built.
 
 # Docs
 echo Building API docs...
-rm -rf sdk_docs
-mkdir sdk_docs
+mkdir sdk_contents/docs
 
 # invoke doxygen
 pushd ../../Docs/src
 doxygen html.cfg
 popd
 
-cp -R ../../Docs/api sdk_docs/
+cp -R ../../Docs/api sdk_contents/docs/
 # delete unnecessary files
-rm -f sdk_docs/api/html/*.hhk
-rm -f sdk_docs/api/html/*.map
-rm -f sdk_docs/api/html/*.md5
-cp -R ../../Docs/manual sdk_docs/
-cp -R ../../Docs/licenses sdk_docs/
-cp -R ../Win32/docs/ReadMe.html sdk_docs/
-cp -R ../../Docs/ChangeLog.html sdk_docs/
+rm -f sdk_contents/docs/api/html/*.hhk
+rm -f sdk_contents/docs/api/html/*.map
+rm -f sdk_contents/docs/api/html/*.md5
+cp -R ../../Docs/manual sdk_contents/docs/
+cp -R ../../Docs/licenses sdk_contents/docs/
+cp -R ../Win32/docs/ReadMe.html sdk_contents/docs/
+cp -R ../../Docs/ChangeLog.html sdk_contents/docs/
 
 echo API generation done.
-echo Building docs package...
-/Developer/Tools/packagemaker -build -proj sdk_docs.pmproj -p sdk_docs.pkg
-echo Docs package done.
+
 # do samples
 echo Copying samples...
-rm -rf sdk_samples
-mkdir sdk_samples
+mkdir sdk_contents/samples
 
 # Copy project location
-cp -R ../../Mac/Samples/* sdk_samples/
+cp -R ../../Mac/Samples/* sdk_contents/samples/
 # copy source
-mkdir sdk_samples/src
-mkdir sdk_samples/include
+mkdir sdk_contents/samples/src
+mkdir sdk_contents/samples/include
 
-find ../../samples -iname *.cpp -exec cp \{\} sdk_samples/src \;
-find ../../samples -iname *.h -exec cp \{\} sdk_samples/include \;
-cp ../../ReferenceApplication/BspCollision/src/*.cpp sdk_samples/src
+find ../../samples -iname *.cpp -exec cp \{\} sdk_contents/samples/src \;
+find ../../samples -iname *.h -exec cp \{\} sdk_contents/samples/include \;
+cp ../../ReferenceApplication/BspCollision/src/*.cpp sdk_contents/samples/src
 
 # Copy dependencies
-mkdir sdk_samples/Dependencies
-mkdir sdk_samples/Dependencies/include
-mkdir sdk_samples/Dependencies/lib
-#mkdir sdk_samples/Dependencies/lib/Debug
-mkdir sdk_samples/Dependencies/lib/Release
-cp -R ../../Dependencies/include/OIS sdk_samples/Dependencies/include
-#cp ../../Dependencies/lib/Debug/libois.a sdk_samples/Dependencies/lib/Debug/
-cp ../../Dependencies/lib/Release/libois.a sdk_samples/Dependencies/lib/Release/
+mkdir sdk_contents/samples/Dependencies
+mkdir sdk_contents/samples/Dependencies/include
+mkdir sdk_contents/samples/Dependencies/lib
+#mkdir sdk_contents/samples/Dependencies/lib/Debug
+mkdir sdk_contents/samples/Dependencies/lib/Release
+cp -R ../../Dependencies/include/OIS sdk_contents/samples/Dependencies/include
+#cp ../../Dependencies/lib/Debug/libois.a sdk_contents/samples/Dependencies/lib/Debug/
+cp ../../Dependencies/lib/Release/libois.a sdk_contents/samples/Dependencies/lib/Release/
 
 # Fix up project references (2 stage rather than in-place since in-place only works for single sed commands)
-sed -f editsamples.sed sdk_samples/Samples.xcodeproj/project.pbxproj > tmp.xcodeproj
-mv tmp.xcodeproj sdk_samples/Samples.xcodeproj/project.pbxproj
-# Change the reference type on Ogre.framework
-patch -p0 < patchsamples.diff
-# Change the include type of the Ogre.framework so we use the Ogre/ prefix since external
-find sdk_samples -iname *.h -exec sed -i -e 's/include "Ogre/include "Ogre\/Ogre/g' \{\} \;
-find sdk_samples -iname *.cpp -exec sed -i -e 's/include "Ogre/include "Ogre\/Ogre/g' \{\} \;
+sed -f editsamples.sed sdk_contents/samples/Samples.xcodeproj/project.pbxproj > tmp.xcodeproj
+mv tmp.xcodeproj sdk_contents/samples/Samples.xcodeproj/project.pbxproj
+
 
 echo Samples copied.
-echo Building samples package...
-/Developer/Tools/packagemaker -build -proj sdk_samples.pmproj -p sdk_samples.pkg
-echo Samples package built.
 
-# Final install
-echo Building final installer...
-# NB can't use the alias for doing dist packages? Always fails!
-# Also note - PackageManager fails when using relative paths to contained packages
-# when building a distribution package. Therefore the paths in the pmproj are absolute
-# but need updating to make sure they are correct
-# Get current dir and escape it (NB backslashes have to be double-escaped here)
-currDir=`pwd | sed -e 's/\//\\\\\\//g'`
-sedCmd="s/\<string\>.*\/ogrenew\/SDK\/OSX\/sdk_frameworks\.pkg\<\/string\>/<string>$currDir\/sdk_frameworks.pkg<\/string>/"
-echo $sedCmd > sed.cmd
-sedCmd="s/\<string\>.*\/ogrenew\/SDK\/OSX\/sdk_docs\.pkg\<\/string\>/<string>$currDir\/sdk_docs.pkg<\/string>/"
-echo $sedCmd >> sed.cmd
-sedCmd="s/\<string\>.*\/ogrenew\/SDK\/OSX\/sdk_samples\.pkg\<\/string\>/<string>$currDir\/sdk_samples.pkg<\/string>/"
-echo $sedCmd >> sed.cmd
-
-# Need to make sure that project file is XML rather than binary
-plutil -convert xml1 sdk_complete_in.pmproj
-sed -f sed.cmd sdk_complete_in.pmproj > sdk_complete.pmproj
-/Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker -build -proj sdk_complete.pmproj -p OgreSDKOSX_v1.4.4.mpkg
 
 echo Done!
