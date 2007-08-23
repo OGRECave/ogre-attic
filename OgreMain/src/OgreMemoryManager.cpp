49cedabe1a96a6cad7f723f47621081238246eb7
c4llidus <c4llidus>
2007-08-23 14:43:54 +0000

    fixed missing defines, added files to vc8 project

--- OgreMain/src/OgreMemoryManager.cpp
+++ OgreMain/src/OgreMemoryManager.cpp
@@ -62,6 +62,15 @@ Ogre::MemoryManager*      Ogre::MemoryManager::smInstance = NULL;
 
 // win32 POSIX(ish) memory emulation
 //--------------------------------------------------------------------
+#define PROT_READ       0x1             // Page can be read.
+#define PROT_WRITE      0x2             // Page can be written.
+#define PROT_EXEC       0x4             // Page can be executed.
+#define PROT_NONE       0x0             // Page can not be accessed.
+
+#define MAP_SHARED      0x01            // Share changes.
+#define MAP_PRIVATE     0x02            // Changes are private.
+#define MAP_ANONYMOUS   0x20            // Don't use a file.
+
 long getpagesize (void) 
 {  
     SYSTEM_INFO system_info;
